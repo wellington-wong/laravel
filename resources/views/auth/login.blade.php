59a358e866fcfa145bac466117a774a17efabbe9
wellington <wellington@exults.com>
2017-04-27 00:37:46 +0800

    Incentful - Add signup navigation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,15 +31,17 @@
             </div>
             <section class="container">
                 <div>
-                    <figure class="logo col-md-6"><img src="/images/logo.png" alt=""></figure>
-                    <ul class="nav navbar-nav navbar-right col-md-6 pull-right">
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                        <li><a href="#">How it Works</a></li>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
+                    <figure class="logo col-md-3"><img src="/images/logo.png" alt=""></figure>
+                    <div class="col-md-9">
+                        <ul class="nav navbar-nav navbar-right navbar-bottom-right">
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="#">How it Works</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         </header>
