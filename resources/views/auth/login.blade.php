a47db7b9e441ef15131e7a9c2f16969f8b4c666c
wellington <wellington@exults.com>
2017-04-27 08:45:14 +0800

    Incentful - Update main content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -67,7 +67,6 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 main-content">
                         <div class="panel panel-default">
-                            <div class="panel-heading">Login</div>
                             <div class="panel-body">
                                 <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                                     {{ csrf_field() }}
