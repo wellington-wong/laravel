9fa47a9626fb6c23ee369ea3f0b0c1b78bc6c3d7
wellington <wellington@exults.com>
2017-04-27 23:02:31 +0800

    Incentful - Install jquery ui as node module

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -86,7 +86,7 @@ class ReferralController extends Controller
     public function autocomplete( Request $request )
     {
 
-        return 123;
+        return view('referral.autocomplete');
     }
 
 }
