8afe028c7483e183c1bdebf25e6e46de04c04c75
wellington <wellington@exults.com>
2017-04-28 19:44:33 +0800

    Incentful - referral program: Add js scripts to login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -198,6 +198,13 @@
             </div> 
         </footer>
         <!-- End Footer -->
+    
+    <!-- Scripts -->
+    <script src="{{ mix('/js/manifest.js') }}"></script>
+    <script src="{{ mix('/js/vendor.js') }}"></script>
+    <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="{{ mix('/js/all.js') }}"></script>
+    {{--<script src="{{ asset('js/app.js') }}"></script>--}}
 
     </body>
 </html>
