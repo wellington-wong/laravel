5b0a65a3f340c7f4582e8784d6e0fe1eafdd10a9
wellington <wellington@exults.com>
2017-04-29 06:24:31 +0800

    Incentful - referral program: Update notification

--- app/Notifications/newReferral.php
+++ app/Notifications/newReferral.php
@@ -41,9 +41,8 @@ class newReferral extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line('A new referral has been made.')
+                    ->action('Go to referral', url('/'));
     }
 
     /**
