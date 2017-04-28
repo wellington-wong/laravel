6e2503a62e110c9f878c5adfcc9ff1ab3305ce64
wellington <wellington@exults.com>
2017-04-28 20:28:08 +0800

    Incentful - referral program: Fix hamburger menu

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -8,14 +8,15 @@
             </div>
             <section class="container">
                 <div class="row">
+                <nav>
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <figure class="logo col-md-3"><img src="/images/logo.png" alt=""></figure>
-                    <div class="col-md-9" id="app-navbar-collapse">
+                    <figure class="logo col-xs-6 col-md-3"><img src="/images/logo.png" alt=""></figure>
+                    <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
                         <ul class="nav navbar-nav navbar-right navbar-bottom-right">
                             <li><a href="#">How it Works</a></li>
                             <li><a href="#">Features</a></li>
@@ -24,6 +25,7 @@
                             <li><a href="#">Contact</a></li>
                         </ul>
                     </div>
+                    </nav>
                 </div>
             </section>
         </header>
