a8d28ae98be7bfa7b07bda323755e82a8ecbbf7d
wellington <wellington@exults.com>
2017-04-28 21:21:52 +0800

    Incentful - referral program: Fix hamburger menu auth views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,11 +29,11 @@
             <div class="row">
 
                 @if (!Auth::guest())
-                <div class="col-xs-3 col-sm-3 col-md-3 col-sidebar">
+                <div class="col-sm-3 col-md-3 col-sidebar">
                 @include('layouts.sidebar')
                 </div>
                 @endif
-                <div class="col-xs-9 col-xs-offset-3 col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 page-right main-content">
+                <div class="col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 page-right main-content">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             @include('layouts.navbar')
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,17 +1,6 @@
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container-fluid">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <div>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
