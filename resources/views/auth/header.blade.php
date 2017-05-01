a348ddb4b4b559b933fba64c95f02aeaa48e0926
wellington <wellington@exults.com>
2017-05-01 18:27:37 +0800

    Incentful - Adjust code indentation

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -8,23 +8,23 @@
             </div>
             <section class="container">
                 <div class="row">
-                <nav>
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <figure class="logo col-xs-6 col-md-3"><img src="/images/logo.png" alt=""></figure>
-                    <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
-                        <ul class="nav navbar-nav navbar-right navbar-bottom-right">
-                            <li><a href="#">How it Works</a></li>
-                            <li><a href="#">Features</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Pricing</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
-                    </div>
+                    <nav>
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <figure class="logo col-xs-6 col-md-3"><img src="/images/logo.png" alt=""></figure>
+                        <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
+                            <ul class="nav navbar-nav navbar-right navbar-bottom-right">
+                                <li><a href="#">How it Works</a></li>
+                                <li><a href="#">Features</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
                     </nav>
                 </div>
             </section>
