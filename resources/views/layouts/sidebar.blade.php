b95c62479fbc04dcc0a9715a4b6bc29d559545ff
wellington <wellington@exults.com>
2017-05-01 19:25:28 +0800

    Incentful - Add hamburger menu to auth page

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,29 +1,37 @@
 
-            <div class="panel panel-default">
-                <div class="panel-heading"><a class="logo" href="{!! url('/') !!}"></a></div>
+            <nav>
+                <button type="button" class="navbar-toggle collapsed sidebar-menu" data-toggle="collapse" data-target="#app-sidebar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="panel panel-default collapse navbar-collapse" id="app-sidebar-collapse">
+                    <div class="panel-heading"><a class="logo" href="{!! url('/') !!}"></a></div>
 
-                <div class="panel-body">
-                    <div class="menu-item">
-                        <a href="{{ route('company-create') }}">Create Company</a>
-                    </div>
-                    <div>Referrals</div>
-                    <div class="menu-item">
-                        <a href="{{ route('referral-create') }}" >Submit a Referral</a>
-                    </div>
-                    <div class="menu-item">
-                        <a href="{{ route('referrals') }}" >Referral History</a>
-                    </div>
-
-                </div>
-
-                <div class="panel-footer">
-                        <div class="menu-separator"></div>
-
+                    <div class="panel-body">
                         <div class="menu-item">
-                            <a href="{{ route('referral-create') }}" >Account Settings</a>
+                            <a href="{{ route('company-create') }}">Create Company</a>
+                        </div>
+                        <div>Referrals</div>
+                        <div class="menu-item">
+                            <a href="{{ route('referral-create') }}" >Submit a Referral</a>
                         </div>
                         <div class="menu-item">
-                            <a href="{{ route('referral-create') }}" >Need Help?</a>
-                        </div>                
+                            <a href="{{ route('referrals') }}" >Referral History</a>
+                        </div>
+
+                    </div>
+
+                    <div class="panel-footer">
+                            <div class="menu-separator"></div>
+
+                            <div class="menu-item">
+                                <a href="{{ route('referral-create') }}" >Account Settings</a>
+                            </div>
+                            <div class="menu-item">
+                                <a href="{{ route('referral-create') }}" >Need Help?</a>
+                            </div>                
+                    </div>
                 </div>
-            </div>
+            </nav>
