86d732eac00fbd0c617a7a32d1ba2cbde286445a
wellington <wellington@exults.com>
2017-05-01 21:06:43 +0800

    Incentful - Update submit referral layout

--- resources/views/forms/phone.blade.php
+++ resources/views/forms/phone.blade.php
@@ -2,5 +2,5 @@
     <label><?php echo isset($phone_label) ? $phone_label : 'Phone Number' ?></label>
     {{ Form::hidden('phone_country', 'US') }}
     {{-- Form::select('phone_country', \Propaganistas\LaravelIntl\Facades\Country::all(), 'US' ) --}}
-    {{ Form::text('phone', null, ['class'=>'form-control bfh-phone', 'data-format'=>'(ddd) ddd-dddd']) }}
+    {{ Form::text('phone', null, ['class'=>'bfh-phone', 'data-format'=>'(ddd) ddd-dddd']) }}
 </div>
