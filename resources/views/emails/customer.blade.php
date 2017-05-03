1787db44f08cae004b278399e1a5548062041173
wellington <wellington@exults.com>
2017-05-04 01:58:23 +0800

    Incentful - Add customer email template

---
+++ resources/views/emails/customer.blade.php
@@ -0,0 +1,13 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
