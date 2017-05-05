10e6601410fe67dd47db033d0b0bec67511aa48e
wellington <wellington@exults.com>
2017-05-05 22:05:32 +0800

    Incentful - Add methods to UserTemplate

--- app/UserTemplate.php
+++ app/UserTemplate.php
@@ -12,4 +12,26 @@ class UserTemplate extends Model
     const TEMPLATE_ID_SUPERADMIN 		= 2;
     const TEMPLATE_ID_ADMIN 			= 3;
     const TEMPLATE_ID_MEMBER 			= 4;
+
+    public function newQuery() {
+        $q = parent::newQuery();
+        if ( Auth::user() ) { //NEED THIS IN CASE A MIGRATION NEEDS TO ACCESS ALL
+            $q->whereIn( 'user_template.subdomain_id', [0, Auth::user()->settings->id ]);
+        }
+        return $q;
+    }
+
+    public static function listUserTemplateNames() {
+
+        return self::select('id', 'name')
+            ->pluck('name', 'id');
+
+    }
+
+    public static function getUserTemplates() {
+
+        return self::select('*')
+            ->get();
+
+    }
 }
