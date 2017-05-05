a889847b89552f77dc86245f01ba21aaf93aab77
wellington <wellington@exults.com>
2017-05-06 03:36:41 +0800

    Incentful - Add seeder to create global admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+    	// Create Global Admin 
+    	if (!$user = User::where('email', 'exults.referral@gmail.com')->first()) {
+	        $user = new User();
+	        $user->first_name = "Global";
+	        $user->last_name = "Admin";
+	        $user->name = $user->first_name . ' ' . $user->last_name;
+	        $user->email = "exults.referral@gmail.com";
+	        $user->password = Hash::make('E*x%u~lts321!');
+	        //$user->subdomain = 1;
+	        $user->usertype = User::USERTYPE_GLOBALADMIN;
+	        $user->user_template_id = 1;
+	        $user->save();
+        }
     }
 }
