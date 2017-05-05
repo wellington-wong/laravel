e3eb289f07bf75b008721520d6e493512e4249cb
wellington <wellington@exults.com>
2017-05-05 21:48:27 +0800

    Incentful - Setup UserTemplate model

--- app/UserTemplate.php
+++ app/UserTemplate.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserTemplate extends Model
 {
-    //
+    protected $table = 'user_templates';
+
+    const TEMPLATE_ID_GLOBALADMIN 	= 1;
+    const TEMPLATE_ID_SUPERADMIN 		= 2;
+    const TEMPLATE_ID_ADMIN 		= 2;
+    const TEMPLATE_ID_MEMBER 		= 2;
 }
