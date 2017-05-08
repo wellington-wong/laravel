6cd73eb85716ccf929fff3d9b937fbbde010e651
wellington <wellington@exults.com>
2017-05-09 05:25:44 +0800

    Incentful - Add db seed for roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,18 +15,29 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
-    	// Create Global Admin 
-    	if (!$user = User::where('email', 'exults.referral@gmail.com')->first()) {
-	        $user = new User();
-	        $user->first_name = "Global";
-	        $user->last_name = "Admin";
-	        $user->name = $user->first_name . ' ' . $user->last_name;
-	        $user->email = "exults.referral@gmail.com";
-	        $user->password = Hash::make('E*x%u~lts321!');
-	        //$user->subdomain = 1;
-	        $user->usertype = User::USERTYPE_GLOBALADMIN;
-	        $user->user_template_id = 1;
-	        $user->save();
-        }
+        $owner = new Role();
+        $owner->name         = 'member';
+        $owner->display_name = 'Member'; // optional
+        $owner->description  = 'User is allowed to create and edit accounts; submit and track referrals'; // optional
+        $owner->save();
+
+        $admin = new Role();
+        $admin->name         = 'admin';
+        $admin->display_name = 'Admin'; // optional
+        $admin->description  = 'User is allowed to submit and change referral statuses; edit and export member information'; // optional
+        $admin->save();
+
+        $admin = new Role();
+        $admin->name         = 'superAdmin';
+        $admin->display_name = 'Super Admin'; // optional
+        $admin->description  = 'User is allowed to submit on behalf of member and change referral statuses; edit and export member information; add/delete admins, defined user roles and add/change billing information'; // optional
+        $admin->save();
+
+        $admin = new Role();
+        $admin->name         = 'globalAdmin';
+        $admin->display_name = 'Global Admin'; // optional
+        $admin->description  = 'User is allowed to login as super admin for all accounts; submit on behalf of member and change referral statuses; export member information; add/delete admins, defined user roles and add/change billing information'; // optional
+        $admin->save();
+
     }
 }
