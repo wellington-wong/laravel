0b2256bf57484116bf26edfe0bfc072ba5a9157f
wellington <wellington@exults.com>
2017-05-09 06:57:40 +0800

    Incentful - Add roles to db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,29 +15,33 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
-        $owner = new Role();
-        $owner->name         = 'member';
-        $owner->display_name = 'Member'; // optional
-        $owner->description  = 'User is allowed to create and edit accounts; submit and track referrals'; // optional
-        $owner->save();
+        // Create Global Admin 
+        if (!$user = User::where('email', 'exults.referral@gmail.com')->first()) {
+            $user = new User();
+            $user->first_name = "Global";
+            $user->last_name = "Admin";
+            $user->name = $user->first_name . ' ' . $user->last_name;
+            $user->email = "exults.referral@gmail.com";
+            $user->password = Hash::make('E*x%u~lts321!');
+            //$user->subdomain = 1;
+            $user->save();
+        }
 
-        $admin = new Role();
-        $admin->name         = 'admin';
-        $admin->display_name = 'Admin'; // optional
-        $admin->description  = 'User is allowed to submit and change referral statuses; edit and export member information'; // optional
-        $admin->save();
-
-        $admin = new Role();
-        $admin->name         = 'superAdmin';
-        $admin->display_name = 'Super Admin'; // optional
-        $admin->description  = 'User is allowed to submit on behalf of member and change referral statuses; edit and export member information; add/delete admins, defined user roles and add/change billing information'; // optional
-        $admin->save();
-
-        $admin = new Role();
-        $admin->name         = 'globalAdmin';
-        $admin->display_name = 'Global Admin'; // optional
-        $admin->description  = 'User is allowed to login as super admin for all accounts; submit on behalf of member and change referral statuses; export member information; add/delete admins, defined user roles and add/change billing information'; // optional
-        $admin->save();
+        // Create user roles if not exist
+        $roleProfiles = array(
+            array('member', 'Member', 'User is allowed to create and edit accounts; submit and track referrals'),
+            array('admin', 'Admin', 'User is allowed to submit and change referral statuses; edit and export member information'),
+            array('superAdmin', 'Super Admin', 'User is allowed to submit on behalf of member and change referral statuses; edit and export member information; add/delete admins, defined user roles and add/change billing information'),
+            array('globalAdmin', 'Global Admin', 'User is allowed to login as super admin for all accounts; submit on behalf of member and change referral statuses; export member information; add/delete admins, defined user roles and add/change billing information'),
+        );
+        foreach ($roleProfiles as $roleProfile) {
+            $fields = array(
+                'name' => $roleProfile[0],
+                'display_name' => $roleProfile[1],
+                'description' => $roleProfile[2],
+            );
+            $role = Role::firstOrCreate($fields);
+        }
 
     }
 }
