cdc30708370d8d4a07694ae740f4c9f02b04a8d5
wellington <wellington@exults.com>
2017-05-09 20:22:33 +0800

    Incentful - referral program: Add gates for user roles and permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,55 +28,55 @@ class AuthServiceProvider extends ServiceProvider
         $user = auth()->user();
 
         // Member
-        Gate::define('create-account', function ($user) {
-            return $user->hasPermission('can_create_account');
+        Gate::define('can-create-account', function ($user) {
+            return $user->can('can_create_account');
         });
 
-        Gate::define('edit-account-details', function (\User $user) {
-            return $user->hasPermission('can_edit_account');
+        Gate::define('can-edit-account-details', function (\User $user) {
+            return $user->can('can_edit_account');
         });
 
-        Gate::define('submit-referral', function (\User $user) {
-            return $user->hasPermission('can_submit_referral');
+        Gate::define('can-submit-referral', function (\User $user) {
+            return $user->can('can_submit_referral');
         });
 
-        Gate::define('track-referral', function (\User $user) {
-            return $user->hasPermission('can_track_referral');
+        Gate::define('can-track-referral', function (\User $user) {
+            return $user->can('can_track_referral');
         });
 
         // Admin, Super Admin, Global Admin
-        Gate::define('submit-member-referral', function (\User $user) {
-            return $user->hasPermission('can_submit_member_referral');
+        Gate::define('can-submit-member-referral', function (\User $user) {
+            return $user->can('can_submit_member_referral');
         });
 
-        Gate::define('edit-member-information', function (\User $user) {
-            return $user->hasPermission('can_edit_member_information');
+        Gate::define('can-edit-member-information', function (\User $user) {
+            return $user->can('can_edit_member_information');
         });
         
-        Gate::define('export-member-information', function (\User $user) {
-            return $user->hasPermission('can_export_member_information');
+        Gate::define('can-export-member-information', function (\User $user) {
+            return $user->can('can_export_member_information');
         });
         
-        Gate::define('change-referral-statuses', function (\User $user) {
-            return $user->hasPermission('can_change_referral_statuses');
+        Gate::define('can-change-referral-statuses', function (\User $user) {
+            return $user->can('can_change_referral_statuses');
         });
         
         // Super Admin, Global Admin
-        Gate::define('add-delete-admin', function (\User $user) {
-            return $user->hasPermission('can_add_delete_admin');
+        Gate::define('can-add-delete-admin', function (\User $user) {
+            return $user->can('can_add_delete_admin');
         });
         
-        Gate::define('define-user-roles', function (\User $user) {
-            return $user->hasPermission('can_define_user_roles');
+        Gate::define('can-define-user-roles', function (\User $user) {
+            return $user->can('can_define_user_roles');
         });
         
-        Gate::define('add-change-billing-information', function (\User $user) {
-            return $user->hasPermission('can_add_change_billing_information');
+        Gate::define('can-add-change-billing-information', function (\User $user) {
+            return $user->can('can_add_change_billing_information');
         });
         
         // Global Admin
-        Gate::define('login-super-admin-all-accounts', function (\User $user) {
-            return $user->hasPermission('can_login_super_admin_all_accounts');
+        Gate::define('can-login-super-admin-all-accounts', function (\User $user) {
+            return $user->can('can_login_super_admin_all_accounts');
         });
 
     }
