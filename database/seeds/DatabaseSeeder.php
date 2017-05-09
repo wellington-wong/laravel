82cadd6a89c6be76efc867c535744ac24249a60c
wellington <wellington@exults.com>
2017-05-09 21:23:32 +0800

    Incentful - referral program: Update db seed, create role and assign default permissions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,19 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
-        // Create Global Admin 
-        if (!$user = User::where('email', 'exults.referral@gmail.com')->first()) {
-            $user = new User();
-            $user->first_name = "Global";
-            $user->last_name = "Admin";
-            $user->name = $user->first_name . ' ' . $user->last_name;
-            $user->email = "exults.referral@gmail.com";
-            $user->password = Hash::make('E*x%u~lts321!');
-            //$user->subdomain = 1;
-            $user->save();
-        }
-
-        // Create user roles if not exist
+        // Create user roles
         $roleProfiles = array(
             array('member', 'Member', 'User is allowed to create and edit accounts; submit and track referrals'),
             array('admin', 'Admin', 'User is allowed to submit and change referral statuses; edit and export member information'),
@@ -41,23 +29,23 @@ class DatabaseSeeder extends Seeder
                 'display_name' => $roleProfile[1],
                 'description' => $roleProfile[2],
             );
-            $role = Role::firstOrCreate($fields);
+            $role[$roleProfile[0]] = Role::firstOrCreate($fields);
         }
 
         // Create user role permissions
         $permissions = array(
-            array('can_create_account', 'Create account', 'A user is allowed to create account.'),
-            array('can_edit_account', 'Edit account', 'A user is allowed to edit account.'),
-            array('can_submit_referral', 'Submit referral', 'A user is allowed to submit referral.'),
-            array('can_track_referral', 'Track referral', 'A user is allowed to track referral.'),
-            array('can_submit_member_referral', 'Submit member referral', 'A user is allowed to submit member referral.'),
-            array('can_edit_member_information', 'Edit member information', 'A user is allowed to edit member information.'),
-            array('can_export_member_information', 'Export member information', 'A user is allowed to export member information.'),
-            array('can_change_referral_statuses', 'Change referral statuses', 'A user is allowed to change referral statuses.'),
-            array('can_add_delete_admin', 'Add/delete admin', 'A user is allowed to add/delete admin.'),
-            array('can_define_user_roles', 'Define user roles', 'A user is allowed to define user roles.'),
-            array('can_add_change_billing_information', 'Change billing information', 'A user is allowed to change billing information.'),
-            array('can_login_super_admin_all_accounts', 'Login as super admin for all accounts', 'A user is allowed to login as super admin for all accounts.'),
+            array('can_create_account', 'Create account', 'A user is allowed to create account.', array('member')),
+            array('can_edit_account', 'Edit account', 'A user is allowed to edit account.', array('member')),
+            array('can_submit_referral', 'Submit referral', 'A user is allowed to submit referral.', array('member')),
+            array('can_track_referral', 'Track referral', 'A user is allowed to track referral.', array('member')),
+            array('can_submit_member_referral', 'Submit member referral', 'A user is allowed to submit member referral.', array('admin', 'superAdmin', 'globalAdmin')),
+            array('can_edit_member_information', 'Edit member information', 'A user is allowed to edit member information.', array('admin', 'superAdmin', 'globalAdmin')),
+            array('can_export_member_information', 'Export member information', 'A user is allowed to export member information.', array('admin', 'superAdmin', 'globalAdmin')),
+            array('can_change_referral_statuses', 'Change referral statuses', 'A user is allowed to change referral statuses.', array('admin', 'superAdmin', 'globalAdmin')),
+            array('can_add_delete_admin', 'Add/delete admin', 'A user is allowed to add/delete admin.', array('superAdmin', 'globalAdmin')),
+            array('can_define_user_roles', 'Define user roles', 'A user is allowed to define user roles.', array('superAdmin', 'globalAdmin')),
+            array('can_add_change_billing_information', 'Change billing information', 'A user is allowed to change billing information.', array('superAdmin', 'globalAdmin')),
+            array('can_login_super_admin_all_accounts', 'Login as super admin for all accounts', 'A user is allowed to login as super admin for all accounts.', array('globalAdmin')),
         );        
         foreach ($permissions as $permission) {
             $fields = array(
@@ -65,7 +53,34 @@ class DatabaseSeeder extends Seeder
                 'display_name' => $permission[1],
                 'description' => $permission[2],
             );
-            $permission = Permission::firstOrCreate($fields);
+            $permissionObj = Permission::firstOrCreate($fields);
+
+            // Assign permission to role according to documentation
+            if (isset($permission[3])) {
+                foreach ($permission[3] as $roleName) {
+                    if (!$role[$roleName]->hasPermission($permissionObj->name)) {
+                        $role[$roleName]->attachPermission($permissionObj);
+                    }
+                }
+            }
+        }
+
+        // Create Global Admin 
+        if (!$user = User::where('email', 'exults.referral@gmail.com')->first()) {
+            $user = new User();
+            $user->first_name = "Global";
+            $user->last_name = "Admin";
+            $user->name = $user->first_name . ' ' . $user->last_name;
+            $user->email = "exults.referral@gmail.com";
+            $user->password = Hash::make('E*x%u~lts321!');
+            //$user->subdomain = 1;
+            $user->save();
+
+            // Assign global admin role to exults.referral@gmail.com
+            $globalAdmin = Role::where('name', 'globalAdmin')->first();
+            if (isset($globalAdmin)) {
+                $user->attachRole($globalAdmin);
+            }
         }
 
     }
