7e52defa022b3f9f9222728df83882e961b205b1
wellington <wellington@exults.com>
2017-05-09 23:59:10 +0800

    Incentful - referral program: Setup ACL gate @can

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,59 +25,57 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        
-        $user = auth()->user();
 
         // Member
-        Gate::define('can-create-account', function ($user) {
-            return $user->can('can_create_account');
+        Gate::define('create-account', function () {
+            return Auth::user()->can('can_create_account');
         });
 
-        Gate::define('can-edit-account-details', function (\User $user) {
-            return $user->can('can_edit_account');
+        Gate::define('edit-account-details', function () {
+            return Auth::user()->can('can_edit_account');
         });
 
-        Gate::define('can-submit-referral', function (\User $user) {
-            return $user->can('can_submit_referral');
+        Gate::define('submit-referral', function () {
+            return Auth::user()->can('can_submit_referral');
         });
 
-        Gate::define('can-track-referral', function (\User $user) {
-            return $user->can('can_track_referral');
+        Gate::define('track-referral', function () {
+            return Auth::user()->can('can_track_referral');
         });
 
         // Admin, Super Admin, Global Admin
-        Gate::define('can-submit-member-referral', function (\User $user) {
-            return $user->can('can_submit_member_referral');
+        Gate::define('submit-member-referral', function () {
+            return Auth::user()->can('can_submit_member_referral');
         });
 
-        Gate::define('can-edit-member-information', function (\User $user) {
-            return $user->can('can_edit_member_information');
+        Gate::define('edit-member-information', function () {
+            return Auth::user()->can('can_edit_member_information');
         });
         
-        Gate::define('can-export-member-information', function (\User $user) {
-            return $user->can('can_export_member_information');
+        Gate::define('export-member-information', function () {
+            return Auth::user()->can('can_export_member_information');
         });
         
-        Gate::define('can-change-referral-statuses', function (\User $user) {
-            return $user->can('can_change_referral_statuses');
+        Gate::define('change-referral-statuses', function () {
+            return Auth::user()->can('can_change_referral_statuses');
         });
         
         // Super Admin, Global Admin
-        Gate::define('can-add-delete-admin', function (\User $user) {
-            return $user->can('can_add_delete_admin');
+        Gate::define('add-delete-admin', function () {
+            return Auth::user()->can('can_add_delete_admin');
         });
         
-        Gate::define('can-define-user-roles', function (\User $user) {
-            return $user->can('can_define_user_roles');
+        Gate::define('define-user-roles', function () {
+            return Auth::user()->can('can_define_user_roles');
         });
         
-        Gate::define('can-add-change-billing-information', function (\User $user) {
-            return $user->can('can_add_change_billing_information');
+        Gate::define('add-change-billing-information', function () {
+            return Auth::user()->can('can_add_change_billing_information');
         });
         
         // Global Admin
-        Gate::define('can-login-super-admin-all-accounts', function (\User $user) {
-            return $user->can('can_login_super_admin_all_accounts');
+        Gate::define('login-super-admin-all-accounts', function () {
+            return Auth::user()->can('can_login_super_admin_all_accounts');
         });
 
     }
