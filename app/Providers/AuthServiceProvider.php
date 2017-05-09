1fcb5d2199168635d143f7cc1c51ac485f09fba7
wellington <wellington@exults.com>
2017-05-10 00:09:42 +0800

    Incentful - referral program: Setup ACL gate @can

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,54 +28,54 @@ class AuthServiceProvider extends ServiceProvider
 
         // Member
         Gate::define('create-account', function () {
-            return Auth::user()->can('can_create_account');
+            return Auth::user()->can('create_account');
         });
 
         Gate::define('edit-account-details', function () {
-            return Auth::user()->can('can_edit_account');
+            return Auth::user()->can('edit_account');
         });
 
         Gate::define('submit-referral', function () {
-            return Auth::user()->can('can_submit_referral');
+            return Auth::user()->can('submit_referral');
         });
 
         Gate::define('track-referral', function () {
-            return Auth::user()->can('can_track_referral');
+            return Auth::user()->can('track_referral');
         });
 
         // Admin, Super Admin, Global Admin
         Gate::define('submit-member-referral', function () {
-            return Auth::user()->can('can_submit_member_referral');
+            return Auth::user()->can('submit_member_referral');
         });
 
         Gate::define('edit-member-information', function () {
-            return Auth::user()->can('can_edit_member_information');
+            return Auth::user()->can('edit_member_information');
         });
         
         Gate::define('export-member-information', function () {
-            return Auth::user()->can('can_export_member_information');
+            return Auth::user()->can('export_member_information');
         });
         
         Gate::define('change-referral-statuses', function () {
-            return Auth::user()->can('can_change_referral_statuses');
+            return Auth::user()->can('change_referral_statuses');
         });
         
         // Super Admin, Global Admin
         Gate::define('add-delete-admin', function () {
-            return Auth::user()->can('can_add_delete_admin');
+            return Auth::user()->can('add_delete_admin');
         });
         
         Gate::define('define-user-roles', function () {
-            return Auth::user()->can('can_define_user_roles');
+            return Auth::user()->can('define_user_roles');
         });
         
         Gate::define('add-change-billing-information', function () {
-            return Auth::user()->can('can_add_change_billing_information');
+            return Auth::user()->can('add_change_billing_information');
         });
         
         // Global Admin
         Gate::define('login-super-admin-all-accounts', function () {
-            return Auth::user()->can('can_login_super_admin_all_accounts');
+            return Auth::user()->can('login_super_admin_all_accounts');
         });
 
     }
