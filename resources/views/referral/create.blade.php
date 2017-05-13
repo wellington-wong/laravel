cd5a2991e1b3108962715c4d57731a9f105df3f7
wellington <wellington@exults.com>
2017-05-13 18:44:30 +0800

    Incentful - referral program: Add title

--- resources/views/referral/create.blade.php
+++ resources/views/referral/create.blade.php
@@ -2,49 +2,58 @@
 
 @section('content')
 
-    @if( !$errors->isEmpty() )
-        <div class="alert alert-warning">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-
+    <div class="container-fluid">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3><strong>Submit a New Referral</strong></h3>
+                </div>
+            </div>
         </div>
-    @endif
+        @if( !$errors->isEmpty() )
+            <div class="alert alert-warning">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
 
-    {{ Form::open(['route'=>'post-referral-create', 'id' => 'referral-create-form']) }}
+            </div>
+        @endif
 
-    <input type="hidden" name="subdomain_id" value="2{{ $subdomain_id }}" >
+        {{ Form::open(['route'=>'post-referral-create', 'id' => 'referral-create-form']) }}
 
-    <div class="form-group" >
-        <label>Referral's First Name</label>
-        {{ Form::text('first_name') }}
+        <input type="hidden" name="subdomain_id" value="2{{ $subdomain_id }}" >
+
+        <div class="form-group" >
+            <label>Referral's First Name</label>
+            {{ Form::text('first_name') }}
+        </div>
+
+        <div class="form-group" >
+            <label>Referral's Last Name</label>
+            {{ Form::text('last_name') }}
+        </div>
+
+        @include('forms.phone', ['phone_label'=>"Referral's Phone Number"])
+
+        <div class="form-group" >
+            <label>Referral's Email</label>
+            {{ Form::text('email', null, array('class' => 'referral-email')) }}
+        </div>
+
+        @include('forms.address')
+
+        <div class="form-group">
+            <input type="checkbox" class="terms-acceptance" >
+            Terms!
+        </div>
+
+
+        <button type="submit" class="terms-button btn btn-primary button-responsive-100">Submit Referral</button>
+
+        {{ Form::close() }}
     </div>
-
-    <div class="form-group" >
-        <label>Referral's Last Name</label>
-        {{ Form::text('last_name') }}
-    </div>
-
-    @include('forms.phone', ['phone_label'=>"Referral's Phone Number"])
-
-    <div class="form-group" >
-        <label>Referral's Email</label>
-        {{ Form::text('email', null, array('class' => 'referral-email')) }}
-    </div>
-
-    @include('forms.address')
-
-    <div class="form-group">
-        <input type="checkbox" class="terms-acceptance" >
-        Terms!
-    </div>
-
-
-    <button type="submit" class="terms-button btn btn-primary button-responsive-100">Submit Referral</button>
-
-    {{ Form::close() }}
 
 @include('layouts.modal')
 
