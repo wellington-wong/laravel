dd8d374950d30526aece9bc2111949680dd01b8e
wellington <wellington@exults.com>
2017-05-13 11:37:07 +0800

    Incentful - referral program: Add company logo placeholder

--- resources/views/company/company.blade.php
+++ resources/views/company/company.blade.php
@@ -12,7 +12,7 @@
 		<div class="panel-body">
 			<div class="row">
 				<div class="col-md-3 col-lg-3 " align="center"> 
-					<img alt="{{ $company->company_name }}" src="/{{ $company->logo }}" class="img-responsive"> 
+					<img alt="{{ $company->company_name }}" src="/{{ isset($company->logo) ? $company->logo : 'images/company-placeholder.png' }}" class="img-responsive"> 
 				</div>                
 
 				<div class=" col-md-9 col-lg-9 "> 
