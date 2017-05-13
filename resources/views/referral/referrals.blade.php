23d9e8c6676f004ec7b43432055a89d0a7bf2faa
wellington <wellington@exults.com>
2017-05-13 19:45:19 +0800

    Incentful - referral program: Update referrals view and css

--- resources/views/referral/referrals.blade.php
+++ resources/views/referral/referrals.blade.php
@@ -2,18 +2,30 @@
 
 @section('content')
 
-    <table class="table table-striped table-hover">
-        <thead><tr>
-            <th>Name</th>
-            <th>Status</th>
-        </tr></thead>
-        @foreach($referrals as $r)
-            <tr>
-                <td>{{ $r->referred->display_name }}</td>
-                <td>{{ \App\Referral::$status[$r->status] }}</td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container-fluid">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4>Your Submitted Referrals</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-hover table-referral">
+                    @foreach($referrals as $r)
+                        <tr>
+                            <td>{{ $r->referred->created_at->format('m/d/y') }}</td>
+                            <td>You referred {{ $r->referred->display_name }}</td>
+                            <td>{{ \App\Referral::$status[$r->status] }}</td>
+                            <td><button class="btn btn-default">view details</button></td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
