adac94e8ed997fd48c05e364fa2227291b4e8cab
wellington <wellington@exults.com>
2017-05-19 21:03:27 +0800

    Incentful - referral program: Add link to account name

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,9 +10,8 @@
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown pull-left">
-                                Hi <a href="#" class="" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} 
-                                </a> <span class="nav-separator">|</span> <a href="{{ route('manage-account') }}">Manage Account</a>
+                                Hi <a href="{{ route('manage-account') }}" class="" role="button" aria-expanded="false">{{ Auth::user()->name }}</a> 
+                                <span class="nav-separator">|</span> <a href="{{ route('manage-account') }}">Manage Account</a>
                             </li>
                             <li class="logout pull-left">
                                 <a href="{{ route('logout') }}"
