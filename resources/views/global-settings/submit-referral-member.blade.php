3751b140a7733411ca73d5db77600bed7ae92bca
wellington <wellington@exults.com>
2017-05-20 21:56:16 +0800

    Incentful - referral program: Update add referral on behalf of member

--- resources/views/global-settings/submit-referral-member.blade.php
+++ resources/views/global-settings/submit-referral-member.blade.php
@@ -23,44 +23,49 @@
 		</div>
 
 	    <div class="row">
-		    <div class="col-md-12">
+		    <div class="col-md-12 referral-create-form-wrapper">
 			    {{ Form::open(['route'=>'post-referral-create', 'id' => 'referral-create-form']) }}
 
 			    <input type="hidden" name="subdomain_id" value="" >
 
-			    <div class="form-group" >
-			        <label>Member</label>
-			        {{ Form::text('member') }}
-					{{ Form::hidden('member_id', '', array('id' => 'member-id')) }}
+			    <div class="form-group col-md-12">
+			        {{ Form::text('member', old('member'), ['placeholder' => 'Member\'s Name', 'class' => 'form-control']) }}
+					{{ Form::hidden('member_id', old('member_id'), ['id' => 'member-id']) }}
 			    </div>
 
-			    <div class="form-group" >
-			        <label>Referral's First Name</label>
-			        {{ Form::text('first_name') }}
+			    <div class="form-group col-md-6">
+			        {{ Form::text('first_name', null , ['placeholder' => 'Referral\'s First Name', 'class' => 'form-control']) }}
 			    </div>
 
-			    <div class="form-group" >
-			        <label>Referral's Last Name</label>
-			        {{ Form::text('last_name') }}
+			    <div class="form-group col-md-6">
+			        {{ Form::text('last_name', null, ['placeholder' => 'Referral\'s Last Name', 'class' => 'form-control']) }}
 			    </div>
 
-			    @include('forms.phone', ['phone_label'=>"Referral's Phone Number"])
+			    @include('forms.phone', ['phone_label'=>'Referral\'s Phone Number', 'placeholder' => 'Referral\'s Phone Number'])
 
-			    <div class="form-group" >
-			        <label>Referral's Email</label>
-			        {{ Form::text('email', null, array('class' => 'referral-email')) }}
+			    <div class="form-group col-md-6">
+			        {{ Form::text('email', old('email'), ['placeholder' => 'Referral\'s Email', 'class' => 'form-control referral-email']) }}
 			    </div>
 
 			    @include('forms.address')
 
-			    <div class="form-group">
-			        <input type="checkbox" class="terms-acceptance" >
-			        Terms!
+			    <div class="form-group col-md-12" >
+			        {{ Form::select('install_complete', ['' => 'Was your friend\'s new AC installation already complete?', 'yes' => 'Yes', 'no' => 'No'], old('install_complete'), ['class' => 'form-control install-complete']) }}
 			    </div>
 
+		          <div class="form-group col-md-12 terms-wrapper">
+		              <input type="checkbox" name="terms[]" class="terms-acceptance pull-left" @if(count($errors)) checked="checked" @endif>
+		              <div class="terms-details">
+		                  I understand that the receipt of the $100.00 Cash Reward is dependent on my referral's AC installation Status.
+		                  I am only entitled for a Referral Reward if/when this referral's AC Unit has been installed by All Year Cooling and Heating, Inc.
+		                  View our full <a href="#" class="terms-condition-link">terms and conditions</a>.
+		              </div>
+		          </div>
 
-			    <button type="submit" class="terms-button btn btn-primary button-responsive-100">Submit Referral</button>
-
+				  <div class="form-group col-md-12 text-center">
+				      <button type="submit" class="terms-button btn btn-primary button-responsive-100 submit-referral">Submit Referral</button>
+				  </div>
+				  
 			    {{ Form::close() }}
 			</div>
 		</div>
