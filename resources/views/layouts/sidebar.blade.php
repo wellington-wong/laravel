c066d0e4162b4a89f890e29cc87e0099ebd3e43d
wellington <wellington@exults.com>
2017-05-23 16:01:31 +0800

    Incentful - referral program: Update sidebard add active class

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -24,27 +24,27 @@
                             -->           
                                 @role(['member'])
                                 <ul class="nav">
-                                    <li>
+                                    <li class="{{ Request::path() == 'referrals' ? 'active' : '' }}">
                                         <div class="main-menu-item-wrapper">
                                             <a href="{{ route('referrals') }}">Referrals</a>
                                             <a href="#submenu-referrals" data-toggle="collapse" class="pull-right menu-marker"><i class="fa fa-minus" aria-hidden="true"></i></a>
                                         </div>
                                         <ul id="submenu-referrals" class="collapse in">
-                                            <li><a href="{{ route('referral-create') }}">Submit a Referral</a></li>
-                                            <li><a href="{{ route('referral-history') }}">Referral History</a></li>
+                                            <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}"><a href="{{ route('referral-create') }}">Submit a Referral</a></li>
+                                            <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}"><a href="{{ route('referral-history') }}">Referral History</a></li>
                                         </ul>
                                     </li>
-                                    <li>
+                                    <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}">
                                         <div class="main-menu-item-wrapper">
                                             <a href="{{ route('referral-rewards') }}">Rewards</a>
                                         </div>
                                     </li>
-                                    <li>
+                                    <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}">
                                         <div class="main-menu-item-wrapper">
                                             <a href="{{ route('how-this-works') }}">How This Works</a>
                                         </div>
                                     </li>
-                                    <li>
+                                    <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}">
                                         <div class="main-menu-item-wrapper">
                                             <a href="{{ route('how-to-get-more-referrals') }}">How to Get More Referrals</a>
                                         </div>
@@ -53,16 +53,16 @@
                                 @endrole
                                 @role(['admin', 'superAdmin', 'globalAdmin'])
                                 <ul class="nav">
-                                    <li>
+                                    <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}">
                                         <div class="main-menu-item-wrapper">
                                             <a href="{{ route('referrals') }}">Referrals</a>
                                             <a href="#submenu-referrals-member" data-toggle="collapse" class="pull-right menu-marker"><i class="fa fa-minus" aria-hidden="true"></i></a>
                                         </div>
                                         <ul id="submenu-referrals-member" class="collapse in">
-                                            <li><a href="{{ route('referral-create') }}">Add Referral</a></li>
+                                            <li class="{{ Request::path() == 'referral-create' ? 'active' : '' }}"><a href="{{ route('referral-create') }}">Add Referral</a></li>
                                         </ul>
                                     </li>
-                                    <li>
+                                    <li class="{{ Request::path() == 'members' ? 'active' : '' }}">
                                         <div class="main-menu-item-wrapper">
                                             <a href="{{ route('members') }}">Members</a>
                                         </div>
@@ -73,12 +73,12 @@
                                             <a href="#program-options" data-toggle="collapse" class="pull-right menu-marker"><i class="fa fa-minus" aria-hidden="true"></i></a>
                                         </div>
                                         <ul id="program-options" class="collapse in">
-                                            @if( isset(auth()->user()->companies[0]->id) )<li><a href="{{ route('get-company', auth()->user()->companies[0]->id) }}">Company Profile</a></li>
-                                            @else<li><a href="{{ route('company-create') }}">Create Company</a></li>@endif
-                                            <li><a href="{{ route('program-options-users') }}">Users</a></li>
-                                            <li><a href="{{ route('program-options-referral-program') }}">Referral Program Settings</a></li>
-                                            <li><a href="{{ route('program-options-reward-settings') }}">Reward Settings</a></li>
-                                            <li><a href="{{ route('program-options-notification-settings') }}">Notification Emails</a></li>
+                                            <li class="{{ Request::path() == 'get-company' ? 'active' : '' }}"><a href="{{ route('get-company', auth()->user()->companies[0]->id) }}">Company Profile</a></li>
+                                            <li class="{{ Request::path() == 'company-create' ? 'active' : '' }}"><a href="{{ route('company-create') }}">Create Company</a></li>
+                                            <li class="{{ Request::path() == 'program-options-users' ? 'active' : '' }}"><a href="{{ route('program-options-users') }}">Users</a></li>
+                                            <li class="{{ Request::path() == 'program-options-referral-program' ? 'active' : '' }}"><a href="{{ route('program-options-referral-program') }}">Referral Program Settings</a></li>
+                                            <li class="{{ Request::path() == 'program-options-reward-settings' ? 'active' : '' }}"><a href="{{ route('program-options-reward-settings') }}">Reward Settings</a></li>
+                                            <li class="{{ Request::path() == 'program-options-notification-settings' ? 'active' : '' }}"><a href="{{ route('program-options-notification-settings') }}">Notification Emails</a></li>
                                         </ul>
                                     </li>
                                     <li>
@@ -87,12 +87,12 @@
                                             <a href="#global-settings" data-toggle="collapse" class="pull-right menu-marker"><i class="fa fa-plus" aria-hidden="true"></i></a>
                                         </div>
                                         <ul id="global-settings" class="collapse">
-                                            @can('submit-member-referral')<li><a href="{{ route('submit-referral-member') }}">Submit Referrals on Behalf of Member</a></li>@endcan
-                                            @can('edit-member-information')<li><a href="{{ route('edit-member-information') }}">Edit Member Information</a></li>@endcan
-                                            @can('export-member-information')<li><a href="{{ route('export-member-information') }}">Export Member Information</a></li>@endcan                                            
-                                            @can('add-delete-admin')<li><a href="{{ route('add-delete-admin') }}">Add/Delete Admin</a></li>@endcan
-                                            @can('define-user-roles')<li><a href="{{ route('define-user-roles') }}">Define User Roles</a></li>@endcan
-                                            @can('login-super-admin-all-accounts')<li><a href="{{ route('login-super-admin') }}">Login as Super Admin</a></li>@endcan
+                                            @can('submit-member-referral')<li class="{{ Request::path() == 'submit-referral-member' ? 'active' : '' }}"><a href="{{ route('submit-referral-member') }}">Submit Referrals on Behalf of Member</a></li>@endcan
+                                            @can('edit-member-information')<li class="{{ Request::path() == 'edit-member-information' ? 'active' : '' }}"><a href="{{ route('edit-member-information') }}">Edit Member Information</a></li>@endcan
+                                            @can('export-member-information')<li class="{{ Request::path() == 'export-member-information' ? 'active' : '' }}"><a href="{{ route('export-member-information') }}">Export Member Information</a></li>@endcan                                            
+                                            @can('add-delete-admin')<li class="{{ Request::path() == 'add-delete-admin' ? 'active' : '' }}"><a href="{{ route('add-delete-admin') }}">Add/Delete Admin</a></li>@endcan
+                                            @can('define-user-roles')<li class="{{ Request::path() == 'define-user-roles' ? 'active' : '' }}"><a href="{{ route('define-user-roles') }}">Define User Roles</a></li>@endcan
+                                            @can('login-super-admin-all-accounts')<li class="{{ Request::path() == 'login-super-admin' ? 'active' : '' }}"><a href="{{ route('login-super-admin') }}">Login as Super Admin</a></li>@endcan
                                         </ul>
                                     </li>
                                 </ul>
