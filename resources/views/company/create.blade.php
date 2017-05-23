efb235e5342a0663ecaa46e58f926da8b7e36fc4
wellington <wellington@exults.com>
2017-05-23 10:13:41 +0800

    Incentful - referral program: Add has error on create company

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -18,11 +18,11 @@
     @include('forms.address', ['company_address' => true])
 
     <div class="form-group col-md-6">
-        {{ Form::text('company_name', old('company_name'), ['placeholder' => 'Company Name', 'class' => 'form-control']) }}
+        {{ Form::text('company_name', old('company_name'), ['placeholder' => 'Company Name', 'class' => 'form-control' . ($errors->has('zip') ? ' has-error' : '')]) }}
     </div>
 
     <div class="form-group col-md-6">
-        {{ Form::text('subdomain', old('company_name'), ['placeholder' => 'Subdomain', 'class' => 'form-control']) }}
+        {{ Form::text('subdomain', old('company_name'), ['placeholder' => 'Subdomain', 'class' => 'form-control' . ($errors->has('zip') ? ' has-error' : '')]) }}
     </div>
 
     @include('forms.phone', ['phone_label'=>'Referral\'s Phone Number', 'placeholder' => 'Referral\'s Phone Number'])
