d692a34f6ef45af2c4a1f06a69d16865927cffcb
wellington <wellington@exults.com>
2017-05-25 22:39:16 +0800

    Incentful - referral program: Update admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,16 @@
 @section('pageTitle', 'Home')
 
 @section('content')
-<div class="container-fluid with-referral-counter">
+<div class="container-fluid with-referral-counter admin-dashboard">
     @include('referral.counter')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-9 dashboard-left cta">
+            <div class="page-header"><h3>Share Your Referral Program</h3></div>
+            <div class="referral-url text-center">
+                <p>This is your referral program's link: <a href="#">refermybiz.businessname.com</a></p>
+            </div>
+        </div>
+        <div class="col-md-3 dashboard-left">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
