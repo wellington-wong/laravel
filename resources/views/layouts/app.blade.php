538c4d4fb5b84ae930da827981b788b289817d83
wellington <wellington@exults.com>
2017-05-25 22:21:51 +0800

    Incentful - referral program: Update admin dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,16 +40,6 @@
                         </div>
 
                         <div class="panel-body">
-                            @if (auth()->user())
-                            <div class="notifications cols-xs-12" data-id="{{ auth()->user()->id }}" data-token="{{ csrf_token() }}">
-                                @foreach ( auth()->user()->unreadNotifications as $notification)
-                                <div class="alert alert-success">
-                                    <div class="col-md-9 message"><strong>{{ isset($notification['data']['duplicate']) ? "Duplicate" : "New" }}</strong> A referral has been submitted: {{ $notification['data']['first_name'] }} {{ $notification['data']['last_name'] }}</div> <div class="col-md-3 text-right"><button type="submit" class="mark-read"  data-nid="{{ $notification->id }}">Mark as read</button></div>
-                                    <div class="clearfix"></div>
-                                </div>
-                                @endforeach
-                            </div>
-                            @endif
                             @yield('content')
                         </div>
                     </div>
