424fa9e178100ba84991eca7d630bc56a1491335
wellington <wellington@exults.com>
2017-05-25 15:33:22 +0800

    Incentful - referral program: Retrieve daterangepicker values

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -422,6 +422,8 @@ $(function (){
 // JQUERY DATERANGEPICKER
 	$('input[name="date_filter"]').daterangepicker({
 		drops: 'up',
-	});
+	}).on('apply.daterangepicker', function(ev, picker) {
+      console.log(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+  	});
 // END JQUERY DATERANGEPICKER
 });
