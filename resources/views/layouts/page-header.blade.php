e7971cacf8d64b0923bc90ca679d9dc0913990d1
wellington <wellington@exults.com>
2017-05-26 15:14:45 +0800

    Incentful - referral program: Update page header

--- resources/views/layouts/page-header.blade.php
+++ resources/views/layouts/page-header.blade.php
@@ -1,8 +1,6 @@
 
         <div class="page-header col-md-{{ $col }}">
-            <div class="row">
-                  <div class="header-title">
-                      <h4>{{ $header }}</h4>
-                  </div>
+            <div class="header-title">
+                <h4>{{ $header }}</h4>
             </div>
        	</div>    
