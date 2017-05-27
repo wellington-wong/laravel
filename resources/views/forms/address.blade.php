f84757cd26b82f98c5a1c59ec7a9fe76aaa0354b
wellington <wellington@exults.com>
2017-05-27 15:41:07 +0800

    Incentful - referral program: Add countries select box

--- resources/views/forms/address.blade.php
+++ resources/views/forms/address.blade.php
@@ -1,46 +1,52 @@
 @if (!isset($company))
-<div class="form-group col-md-6">
+<div class="form-group col-lg-6">
     {{ Form::text('address', null, ['placeholder' => 'Referral\'s Home Address', 'class' => 'form-control' . ($errors->has('address') ? ' has-error' : '')]) }}
 </div>
 
-<div class="form-group col-md-6">
+<div class="form-group col-lg-6">
     {{ Form::text('address2', null, ['placeholder' => 'Address Line 2', 'class' => 'form-control' . ($errors->has('address2') ? ' has-error' : '')]) }}
 </div>
 
-<div class="form-group col-md-4">
+<div class="form-group col-lg-4">
     {{ Form::text('city', null, ['placeholder' => 'City', 'class' => 'form-control' . ($errors->has('city') ? ' has-error' : '')]) }}
 </div>
 
-<div class="form-group col-md-2">
+<div class="form-group col-lg-2">
 	@include('forms.states', [])
 </div>
 
-<div class="form-group col-md-6">
+<div class="form-group col-lg-6">
     {{ Form::text('zip', null, ['placeholder' => 'Zip Code', 'class' => 'form-control' . ($errors->has('zip') ? ' has-error' : '')]) }}
 </div>
 @else 
-<div class="form-group col-md-6">
+<!-- Company Profile -->
+<div class="form-group col-lg-6">
 	<label>Company Address Line 1</label>
     {{ Form::text('address', null, ['placeholder' => 'Company Address Line 1', 'class' => 'form-control' . ($errors->has('address') ? ' has-error' : '')]) }}
 </div>
 
-<div class="form-group col-md-6">
+<div class="form-group col-lg-6">
 	<label>Address Line 2</label>
     {{ Form::text('address2', null, ['placeholder' => 'Line 2', 'class' => 'form-control' . ($errors->has('address2') ? ' has-error' : '')]) }}
 </div>
 
-<div class="form-group col-md-4">
+<div class="form-group col-lg-4">
 	<label>City</label>
     {{ Form::text('city', null, ['placeholder' => 'City', 'class' => 'form-control' . ($errors->has('city') ? ' has-error' : '')]) }}
 </div>
 
-<div class="form-group col-md-2">
+<div class="form-group col-lg-2">
 	<label></label>
 	@include('forms.states', [])
 </div>
 
-<div class="form-group col-md-6">
+<div class="form-group col-lg-2">
 	<label>Zip Code</label>
     {{ Form::text('zip', null, ['placeholder' => 'Zip Code', 'class' => 'form-control' . ($errors->has('zip') ? ' has-error' : '')]) }}
+</div>
+
+<div class="form-group col-lg-4">
+    <label></label>
+    @include('forms.countries', [])
 </div>
 @endif 
