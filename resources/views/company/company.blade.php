5118422ff5560cf165dd8d265f8974333f4218d1
wellington <wellington@exults.com>
2017-05-27 13:30:52 +0800

    Incentful - referral program: Update company create style

--- resources/views/company/company.blade.php
+++ resources/views/company/company.blade.php
@@ -8,42 +8,51 @@
 
         @include('layouts.page-header', ['header' => ucwords($company->company_name), 'col' => 12])
 
-        {{ Form::open(['route'=>'post-company-update', 'id' => 'update-company-form', 'enctype' => 'multipart/form-data']) }}
+        <div class="col-md-8 no-padding-lr">
+	        {{ Form::open(['route'=>'post-company-update', 'id' => 'update-company-form', 'enctype' => 'multipart/form-data']) }}
 
-        <div class="col-md-6">
-        	<label>Company Name</label>
-        	{{ Form::text('company_name', old('company_name'), ['placeholder' => 'Company Name', 'class' => 'form-control' . ($errors->has('company_name') ? ' has-error' : '')]) }}
-        </div>
+	        <div class="col-md-6">
+	        	<label>Company Name</label>
+	        	{{ Form::text('company_name', old('company_name'), ['placeholder' => 'Company Name', 'class' => 'form-control' . ($errors->has('company_name') ? ' has-error' : '')]) }}
+	        </div>
 
-        <div class="col-md-6">
-        	<label>Company Number</label>
-        	@include('forms.phone', ['phone_label'=>'Company  Number', 'placeholder' => 'Company Number', 'no_col' => true])
-        </div>
+	        <div class="col-md-6">
+	        	<label>Company Number</label>
+	        	@include('forms.phone', ['phone_label'=>'Company  Number', 'placeholder' => 'Company Number', 'no_col' => true])
+	        </div>
 
-        <div class="form-group col-md-6" >
-        	<label>Company Email</label>
-        	<i class="fa fa-question-circle-o tooltip-q" aria-hidden="true" data-toggle="tooltip" title="Lorem ipsum dolor sit amet, mea audiam philosophia ne, ex tamquam inimicus eos. Labore contentiones quo ne, quo epicuri voluptua ei"></i>
-            {{ Form::text('company_email', old('company_email'), ['placeholder' => 'Company Email', 'class' => 'form-control company-email' . ($errors->has('company_email') ? ' has-error' : '') ]) }}            
-        </div>
+	        <div class="form-group col-md-6" >
+	        	<label>Company Email</label>
+	        	<i class="fa fa-question-circle-o tooltip-q" aria-hidden="true" data-toggle="tooltip" title="Lorem ipsum dolor sit amet, mea audiam philosophia ne, ex tamquam inimicus eos. Labore contentiones quo ne, quo epicuri voluptua ei"></i>
+	            {{ Form::text('company_email', old('company_email'), ['placeholder' => 'Company Email', 'class' => 'form-control company-email' . ($errors->has('company_email') ? ' has-error' : '') ]) }}            
+	        </div>
 
-        <div class="form-group col-md-6" >
-        	<label>Company Website</label>        	
-            {{ Form::text('company_website', old('company_website'), ['placeholder' => 'Company Website', 'class' => 'form-control company-website' . ($errors->has('company_website') ? ' has-error' : '') ]) }}            
-        </div>
+	        <div class="form-group col-md-6" >
+	        	<label>Company Website</label>        	
+	            {{ Form::text('company_website', old('company_website'), ['placeholder' => 'Company Website', 'class' => 'form-control company-website' . ($errors->has('company_website') ? ' has-error' : '') ]) }}            
+	        </div>
 
-        <div class="form-group col-md-6" >
-        	<label>Company Address Line 1</label>        	
-            {{ Form::text('company_address1', old('company_address1'), ['placeholder' => 'Company Address Line 1', 'class' => 'form-control company-address1' . ($errors->has('company_address1') ? ' has-error' : '') ]) }}            
-        </div>
+	        <div class="form-group col-md-6" >
+	        	<label>Company Address Line 1</label>        	
+	            {{ Form::text('company_address1', old('company_address1'), ['placeholder' => 'Company Address Line 1', 'class' => 'form-control company-address1' . ($errors->has('company_address1') ? ' has-error' : '') ]) }}            
+	        </div>
 
-        <div class="form-group col-md-6" >
-        	<label>Line 2</label>        	
-            {{ Form::text('company_address2', old('company_address2'), ['placeholder' => 'Line 2', 'class' => 'form-control company-address2' . ($errors->has('company_address2') ? ' has-error' : '') ]) }}            
-        </div>
-        
-        @include('forms.address')
+	        <div class="form-group col-md-6" >
+	        	<label>Line 2</label>        	
+	            {{ Form::text('company_address2', old('company_address2'), ['placeholder' => 'Line 2', 'class' => 'form-control company-address2' . ($errors->has('company_address2') ? ' has-error' : '') ]) }}            
+	        </div>
 
-    	{{ Form::close() }}
+	        @include('forms.address')
+
+	        <div class="form-group col-md-12 text-right">
+	        	{{ Form::button('Update', ['class' => 'btn btn-primary button-responsive-100']) }}
+	        </div>
+
+	    	{{ Form::close() }}
+    	</div>
+
+
+
 		<div class="col-xs-12">      
 			<div class="panel panel-info">
 				<div class="panel-heading">
