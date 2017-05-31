6af972482c17fdbea275eafa20b78042b88420e5
wellington <wellington@exults.com>
2017-05-31 09:20:58 +0800

    Incentful - referral program: Update members title

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -7,7 +7,7 @@
         <div class="page-header">
             <div class="row">
                 <div class="col-md-12">
-                    <h4>Your Submitted Referrals</h4>
+                    <h4>Members</h4>
                 </div>
             </div>
         </div>
