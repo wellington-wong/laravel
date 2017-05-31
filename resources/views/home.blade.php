059e2d2347f70b9b46cfdb610bf079e7f2229b44
wellington <wellington@exults.com>
2017-05-31 21:43:43 +0800

    Incentful - referral program: Fix members dashboard error exception

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
             <div class="dashboard-left cta">
                 <div class="page-header"><h3>Share Your Referral Program</h3></div>
                 <div class="referral-url text-center">
-                    <p>This is your referral program's link: <a href="#">{{ auth()->user()->companies()->first()->subdomain }}.businessname.com</a></p>
+                    <p>This is your referral program's link: <a href="#">{{ isset(auth()->user()->companies()->first()->subdomain) ? auth()->user()->companies()->first()->subdomain : '' }}.businessname.com</a></p>
                     <p class="social-links">
                         <a href="http://www.facebook.com/sharer.php?u={{ url('/') }}" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
                         <a href="https://twitter.com/share?url={{ url('/') }}&amp;text={{ auth()->user()->name }}%20Dashboard&amp;hashtags=referralbiz" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
@@ -40,17 +40,17 @@
         <div class="col-md-4 dashboard-right">
             <div class="company-info text-center">            
                 <div class="company-logo">
-                    <img alt="{{ auth()->user()->companies()->first()->company_name }}" src="/{{ isset($company->logo) ? $company->logo : 'images/company-placeholder.png' }}" class="img-responsive col-xs-10 col-xs-offset-1"> 
+                    <img alt="{{ isset(auth()->user()->companies()->first()->company_name) ? auth()->user()->companies()->first()->company_name : '' }}" src="/{{ isset($company->logo) ? $company->logo : 'images/company-placeholder.png' }}" class="img-responsive col-xs-10 col-xs-offset-1"> 
                 </div>
                 <div class="clearfix"></div>
-                <div class="company-info-name">{{ auth()->user()->companies()->first()->company_name }}</div>
+                <div class="company-info-name">{{ isset(auth()->user()->companies()->first()->company_name) ? auth()->user()->companies()->first()->company_name : '' }}</div>
                 <div class="membership-role">
                     <h4><strong>Membership Role</strong></h4>
-                    <span>{{ auth()->user()->roles->first()->display_name  }}</span>
+                    <span>{{ isset(auth()->user()->roles->first()->display_name) ? auth()->user()->roles->first()->display_name : '' }}</span>
                 </div>
                 <div class="program-url">
                     <h5>Program URL</h5>
-                    <span><a href="#">{{ auth()->user()->companies()->first()->subdomain }}.businessname.com</a></span>
+                    <span><a href="#">{{ isset(auth()->user()->companies()->first()->subdomain) ? auth()->user()->companies()->first()->subdomain : '' }}.businessname.com</a></span>
                 </div>
                 <button class="btn btn-primary">Account Settings</button>
             </div>
