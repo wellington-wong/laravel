6d3eb8304e22422dc40c25fcd318b8d9cba9ce06
wellington <wellington@exults.com>
2017-06-09 05:21:55 +0800

    #237118: Incentful - referral program - Update create referral style

--- resources/views/referral/referrals.blade.php
+++ resources/views/referral/referrals.blade.php
@@ -9,28 +9,30 @@
 
         <div class="col-md-12 text-right export-link"><a href="{{ route('referrals-export') . '?' . Request::getQueryString() }}">Export</a></div>
 
-        @include('layouts.page-header', ['header' => 'Referrals', 'col' => 3])
+        <div class="row">
+            @include('layouts.page-header', ['header' => 'Referrals', 'col' => 3])
 
-        <div class="col-md-9 page-filters no-padding-lr">
-            <div class="row">
-                <div class="col-md-4 filter-item search">
-                    <i class="fa fa-search" aria-hidden="true"></i>
-                    {{ Form::text('q', old('q'), ['placeholder' => 'Search', 'class' => 'form-control text', 'data-query' => (isset($param->q) ? $param->q : '')]) }}            
-                    {{ Form::submit('Search', ['placeholder' => 'Search', 'class' => 'btn btn-search']) }}
-                </div>
-                <div class="col-md-4 filter-item date-range">
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    {{ Form::text('daterange', old('daterange'), ['class' => 'form-control text', 'data-query' => (isset($param->daterange) ? $param->daterange : '')] ) }}            
-                    <i class="fa fa-angle-down" aria-hidden="true"></i>          
-                </div>
-                <div class="col-md-4 filter-item filter-by">                    
-                    <div class="form-control text" data-toggle="dropdown">{{ (null !== app('request')->get('status')) ? \App\Referral::$status[app('request')->get('status')] : 'Filter By' }}</div>
-                    <i class="fa fa-angle-down" aria-hidden="true"></i>          
-                    <ul class="dropdown-menu">
-                        @foreach ($referralStatus as $key => $status)
-                            <li><a href="javascript:void(0)" data-id="{{ $status }}" data-query="{{ (isset($param->status) ? $param->status : '') }}" data-status="{{ $key }}">{{ \App\Referral::$status[$status] }}</a></li>
-                        @endforeach
-                    </ul>
+            <div class="col-md-9 page-filters no-padding-lr">
+                <div class="row">
+                    <div class="col-md-4 filter-item search">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        {{ Form::text('q', old('q'), ['placeholder' => 'Search', 'class' => 'form-control text', 'data-query' => (isset($param->q) ? $param->q : '')]) }}            
+                        {{ Form::submit('Search', ['placeholder' => 'Search', 'class' => 'btn btn-search']) }}
+                    </div>
+                    <div class="col-md-4 filter-item date-range">
+                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                        {{ Form::text('daterange', old('daterange'), ['class' => 'form-control text', 'data-query' => (isset($param->daterange) ? $param->daterange : '')] ) }}            
+                        <i class="fa fa-angle-down" aria-hidden="true"></i>          
+                    </div>
+                    <div class="col-md-4 filter-item filter-by">                    
+                        <div class="form-control text" data-toggle="dropdown">{{ (null !== app('request')->get('status')) ? \App\Referral::$status[app('request')->get('status')] : 'Filter By' }}</div>
+                        <i class="fa fa-angle-down" aria-hidden="true"></i>          
+                        <ul class="dropdown-menu">
+                            @foreach ($referralStatus as $key => $status)
+                                <li><a href="javascript:void(0)" data-id="{{ $status }}" data-query="{{ (isset($param->status) ? $param->status : '') }}" data-status="{{ $key }}">{{ \App\Referral::$status[$status] }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
