22ff8855dc94d6e57769eefa2d7da2ca5c34b738
wellington <wellington@exults.com>
2017-06-08 22:54:50 +0800

    #237118: Incentful - referral program - Add phone to company profile

--- resources/views/forms/phone.blade.php
+++ resources/views/forms/phone.blade.php
@@ -3,7 +3,7 @@
     {{ Form::hidden('phone_country', 'US') }}
     {{-- Form::select('phone_country', \Propaganistas\LaravelIntl\Facades\Country::all(), 'US' ) --}}
     @if (isset($class)) <div class="{{ isset($class) ? $class : '' }} col-md-12"> @endif
-    {{ Form::text('phone', null, ['class'=>'bfh-phone form-control' . (isset($placeholder) ? ' hidden' : ''), 'data-format'=>'(ddd) ddd-dddd']) }}
+    {{ Form::text('phone', isset($value) ? $value : null, ['class'=>'bfh-phone form-control' . (isset($placeholder) ? ' hidden' : ''), 'data-format'=>'(ddd) ddd-dddd']) }}
     @if (isset($placeholder)) {{ Form::text('phone_placeholder', null, ['placeholder' => $placeholder, 'class' => 'phone-placeholder form-control' . ($errors->has('phone') ? ' has-error' : '')]) }} @endif
     @if (isset($class)) </div> @endif
 </div>
