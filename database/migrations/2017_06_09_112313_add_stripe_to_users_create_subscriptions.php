fe951e3206031efe78e95fefb5fea21294d966fd
wellington <wellington@exults.com>
2017-06-09 19:29:57 +0800

    #237118: Incentful - referral program - Add stripe columns to users table, create subscriptions table

---
+++ database/migrations/2017_06_09_112313_add_stripe_to_users_create_subscriptions.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStripeToUsersCreateSubscriptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('users', function ($table) {
+            $table->string('stripe_id')->after('remember_token')->nullable();
+            $table->string('card_brand')->after('stripe_id')->nullable();
+            $table->string('card_last_four')->after('card_brand')->nullable();
+            $table->timestamp('trial_ends_at')->after('card_last_four')->nullable();
+        });
+
+        Schema::create('subscriptions', function ($table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('stripe_id');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('users', function ($table) {
+            if(Schema::hasColumn('users', 'stripe_id')) {
+                $table->dropColumn('stripe_id');
+            }
+            if(Schema::hasColumn('users', 'card_brand')) {
+                $table->dropColumn('card_brand');
+            }
+            if(Schema::hasColumn('users', 'card_last_four')) {
+                $table->dropColumn('card_last_four');
+            }
+            if(Schema::hasColumn('users', 'trial_ends_at')) {
+                $table->dropColumn('trial_ends_at');
+            }
+        });
+        Schema::dropIfExists('subscriptions');
+
+    }
+}
