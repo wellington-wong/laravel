1ccfff63091795c2a1ee70230df24df0c04079ea
wellington <wellington@exults.com>
2017-06-09 19:31:01 +0800

    #237118: Incentful - referral program - Update vendor.js

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -10261,7 +10261,7 @@ return jQuery;
 
 /***/ }),
 
-/***/ 178:
+/***/ 181:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(1);
@@ -10269,4 +10269,4 @@ module.exports = __webpack_require__(1);
 
 /***/ })
 
-},[178]);
+},[181]);
