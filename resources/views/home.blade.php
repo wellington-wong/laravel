4f2c85220b346dec7ed372fb864caf4f2f32fde9
wellington <wellington@exults.com>
2017-06-12 20:06:19 +0800

    #237118: Incentful - referral program - Add pencil icon to dashboard company logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,8 +41,8 @@
         <div class="col-md-4 dashboard-right">
             <div class="company-info text-center">            
                 <div class="company-logo">
-                    <i class="fa fa-pencil"></i>
                     <img alt="{{ isset(auth()->user()->companies()->first()->company_name) ? auth()->user()->companies()->first()->company_name : '' }}" src="/{{ isset($company->logo) ? $company->logo : 'images/company-placeholder.png' }}" class="img-responsive col-xs-10 col-xs-offset-1"> 
+                    <div class="dashboard-pencil"><i class="fa fa-pencil"></i></div>
                 </div>
                 <div class="clearfix"></div>
                 <div class="company-info-name">{{ isset(auth()->user()->companies()->first()->company_name) ? auth()->user()->companies()->first()->company_name : '' }}</div>
