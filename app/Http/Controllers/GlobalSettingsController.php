83b3433ef4c6b2548d5a76b147fd0a078aed9f47
wellington <wellington@exults.com>
2017-06-12 22:15:57 +0800

    #237118: Incentful - referral program - Update global settings controller

--- app/Http/Controllers/GlobalSettingsController.php
+++ app/Http/Controllers/GlobalSettingsController.php
@@ -8,38 +8,83 @@ use App\User;
 class GlobalSettingsController extends Controller
 {
 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    /**
+     * Show submit referral member
+     *
+     * @return view
+     */
     public function submitReferralMember(Request $request) 
     {
         return view('global-settings.submit-referral-member');
     }
 
+    /**
+     * Show edit member information
+     *
+     * @return view
+     */
     public function editMemberInformation(Request $request) 
     {
         return view('global-settings.edit-member-information');
     }
 
+    /**
+     * Show export member information
+     *
+     * @return view
+     */
     public function exportMemberInformation(Request $request) 
     {
         return view('global-settings.export-member-information');
     }
 
+    /**
+     * Show add/delete admin
+     *
+     * @return view
+     */
     public function addDeleteAdmin(Request $request) 
     {
         return view('global-settings.add-delete-admin');
     }
 
+    /**
+     * Show define user roles
+     *
+     * @return view
+     */
     public function defineUserRoles(Request $request) 
     {
         return view('global-settings.define-user-roles');
     }
 
+    /**
+     * Show login as any super admin
+     *
+     * @return view
+     */
     public function loginSuperAdmin(Request $request) 
     {
         return view('global-settings.login-super-admin');
+    }
+
+    /**
+     * Show login as user
+     *
+     * @return view
+     */
+    public function loginAsUser(Request $request) 
+    {
+        return view('global-settings.login-as-user');
     }
 }
