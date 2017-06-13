2814db35525b0d7bad2f60d73a1d5056fd941938
wellington <wellington@exults.com>
2017-06-13 23:23:32 +0800

    #237118: Incentful - referral program - Change default order of referrals to latest first

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -74,7 +74,7 @@ class ReferralController extends Controller
             $param = $referrals->getParams();
             $referrals = $referrals->filterSortReferrals(15);
         } else {
-            $referrals = $request->user()->referrals()->paginate(15);
+            $referrals = $request->user()->referrals()->orderBy('id', 'desc')->paginate(15);
         }
 
         return view('referral.referrals')
