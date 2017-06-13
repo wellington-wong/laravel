b94a1899c45119e672e030e37664df4bf0e77c36
wellington <wellington@exults.com>
2017-06-14 04:50:26 +0800

    #237118: Incentful - referral program - Update basic pages

--- resources/views/basic/features.blade.php
+++ resources/views/basic/features.blade.php
@@ -11,19 +11,19 @@
                     <div class="col-md-12 main-content">
                         <div class="panel panel-default">
                             <div class="panel-header">
-									<h3>Features</h3>
+				    <h3>Features</h3>
                             </div>
                             <div class="panel-body">
                             	<p>Lorem ipsum dolor sit amet, mea audiam philosophia ne, ex tamquam inimicus eos. Labore contentiones quo ne, quo epicuri voluptua ei. Cum modo inani ex, eu vidit ipsum sadipscing mei. Mel te reque ornatus oportere, ius maiorum mediocrem iudicabit ne. Iriure dolorum complectitur usu cu, primis deseruisse et duo.</p>
-								<p>Pro adhuc aliquip numquam et. Id vim meis vidisse luptatum, vix case fastidii no. Ponderum cotidieque appellantur usu ex, legimus reprehendunt nam ex, at bonorum fabulas definitionem mei. Nec solet fabulas ocurreret te, scripta similique moderatius nec et. In regione detracto qui, et postea nemore labores per. Ad tation ubique sensibus sea, eum laudem eruditi fierent id, quaestio assentior elaboraret eam id. Veri vivendo appetere sea no.</p>
-								<p>Labitur complectitur vel ea. Vis falli oportere ei, assentior consectetuer vel ei. Id sea dictas meliore. Ut eam fabulas periculis. Populo pericula vel ea, nulla doctus deleniti vis ea, sea ut partiendo patrioque iracundia.</p>
-								<p>Vide definitiones ex eam. Pro mazim propriae ex, et sea purto omittantur, pri in stet eligendi mandamus. Te cum eirmod debitis persecuti. Usu odio quando luptatum in. Alienum quaerendum neglegentur an per, odio vide exerci eu ius, fabellas petentium vix et. Vel eu quod essent, no eos aeque sapientem facilisis.</p>
-								<p>Cu rationibus sadipscing eam, ut ius vero aperiam, quo ne nostro labitur consulatu. Cu vim assum ignota similique, eius comprehensam mei ea. Ut decore aeterno aperiri usu, at usu illud persecuti, solet nonumes adversarium ex vel. Cum eu voluptaria persequeris, in mei quem fabulas, est luptatum adipiscing cu. Mel at dolorum admodum, ea malorum omnesque disputando mei, mel eruditi voluptua ne.</p>
-			    			</div>
-			    		</div>
-			    	</div>
-    			</div>
+        			<p>Pro adhuc aliquip numquam et. Id vim meis vidisse luptatum, vix case fastidii no. Ponderum cotidieque appellantur usu ex, legimus reprehendunt nam ex, at bonorum fabulas definitionem mei. Nec solet fabulas ocurreret te, scripta similique moderatius nec et. In regione detracto qui, et postea nemore labores per. Ad tation ubique sensibus sea, eum laudem eruditi fierent id, quaestio assentior elaboraret eam id. Veri vivendo appetere sea no.</p>
+        			<p>Labitur complectitur vel ea. Vis falli oportere ei, assentior consectetuer vel ei. Id sea dictas meliore. Ut eam fabulas periculis. Populo pericula vel ea, nulla doctus deleniti vis ea, sea ut partiendo patrioque iracundia.</p>
+        			<p>Vide definitiones ex eam. Pro mazim propriae ex, et sea purto omittantur, pri in stet eligendi mandamus. Te cum eirmod debitis persecuti. Usu odio quando luptatum in. Alienum quaerendum neglegentur an per, odio vide exerci eu ius, fabellas petentium vix et. Vel eu quod essent, no eos aeque sapientem facilisis.</p>
+        			<p>Cu rationibus sadipscing eam, ut ius vero aperiam, quo ne nostro labitur consulatu. Cu vim assum ignota similique, eius comprehensam mei ea. Ut decore aeterno aperiri usu, at usu illud persecuti, solet nonumes adversarium ex vel. Cum eu voluptaria persequeris, in mei quem fabulas, est luptatum adipiscing cu. Mel at dolorum admodum, ea malorum omnesque disputando mei, mel eruditi voluptua ne.</p>
+			     </div>
+			 </div>
+			</div>
     		</div>
+    	</div>
         </main>
         <!-- End Main -->
 <!-- Start Footer -->
