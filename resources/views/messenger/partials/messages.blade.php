de94dd5592882036a564ea1e0443c05fee940d2b
wellington <wellington@exults.com>
2017-06-16 04:55:35 +0800

    #237118: Incentful - referral program - Update show message view

--- resources/views/messenger/partials/messages.blade.php
+++ resources/views/messenger/partials/messages.blade.php
@@ -5,7 +5,7 @@
     </a>
     <div class="media-body">
         <h5 class="media-heading">{{ $message->user->name }}</h5>
-        <p>{{ $message->body }}</p>
+        <p>{!! $message->body !!}</p>
         <div class="text-muted">
             <small>Posted {{ $message->created_at->diffForHumans() }}</small>
         </div>
