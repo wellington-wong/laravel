b4c9cffffd14f0e5c16cafc49fa8e19a79b4c1bb
wellington <wellington@exults.com>
2017-06-16 04:58:01 +0800

    #237118: Incentful - referral program - Update show message view

--- resources/views/messenger/partials/messages.blade.php
+++ resources/views/messenger/partials/messages.blade.php
@@ -1,8 +1,7 @@
 <div class="media">
-    <a class="pull-left" href="#">
-        <img src="//www.gravatar.com/avatar/{{ md5($message->user->email) }} ?s=64"
-             alt="{{ $message->user->name }}" class="img-circle">
-    </a>
+    <div class="pull-left">
+        <i class="fa fa-envelope"></i>
+    </div>
     <div class="media-body">
         <h5 class="media-heading">{{ $message->user->name }}</h5>
         <p>{!! $message->body !!}</p>
