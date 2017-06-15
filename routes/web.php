8daf2b3670e9a9481e60b59a883e9c0191c996cb
wellington <wellington@exults.com>
2017-06-15 20:55:10 +0800

    #237118: Incentful - referral program - Add messaging routes

--- routes/web.php
+++ routes/web.php
@@ -107,3 +107,13 @@ Route::get('/api/search', [
 	'as' => 'api/search',
 	'uses' => 'Api\SearchController@search'
 ]);
+
+// Laravel messenger
+// https://github.com/cmgmyr/laravel-messenger
+Route::group(['prefix' => 'messages'], function () {
+    Route::get('/', ['as' => 'messages', 'uses' => 'MessagesController@index']);
+    Route::get('create', ['as' => 'messages.create', 'uses' => 'MessagesController@create']);
+    Route::post('/', ['as' => 'messages.store', 'uses' => 'MessagesController@store']);
+    Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
+    Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
+});
