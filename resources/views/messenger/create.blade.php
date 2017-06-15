3697cb3dd7298f070e8b119b8a0df6a097624a87
wellington <wellington@exults.com>
2017-06-16 04:29:14 +0800

    #237118: Incentful - referral program - Update messages index and create new

--- resources/views/messenger/create.blade.php
+++ resources/views/messenger/create.blade.php
@@ -1,36 +1,47 @@
-@extends('layouts.messages')
+@extends('layouts.app')
+
+@section('pageTitle', 'New Message')
 
 @section('content')
-    <h1>Create a new message</h1>
-    <form action="{{ route('messages.store') }}" method="post">
-        {{ csrf_field() }}
-        <div class="col-md-6">
-            <!-- Subject Form Input -->
-            <div class="form-group">
-                <label class="control-label">Subject</label>
-                <input type="text" class="form-control" name="subject" placeholder="Subject"
-                       value="{{ old('subject') }}">
-            </div>
+    <div class="container-fluid messages-wrapper">
 
-            <!-- Message Form Input -->
-            <div class="form-group">
-                <label class="control-label">Message</label>
-                <textarea name="message" class="form-control">{{ old('message') }}</textarea>
-            </div>
-
-            @if($users->count() > 0)
-                <div class="checkbox">
-                    @foreach($users as $user)
-                        <label title="{{ $user->name }}"><input type="checkbox" name="recipients[]"
-                                                                value="{{ $user->id }}">{!!$user->name!!}</label>
-                    @endforeach
-                </div>
-            @endif
-    
-            <!-- Submit Form Input -->
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary form-control">Submit</button>
-            </div>
+        <div class="row">
+            @include('layouts.page-header', ['header' => 'New Message', 'col' => 6])
         </div>
-    </form>
+
+        <div class="clearfix"></div>
+
+        <h1>Create a new message</h1>
+        <form action="{{ route('messages.store') }}" method="post">
+            {{ csrf_field() }}
+            <div class="col-md-6">
+                <!-- Subject Form Input -->
+                <div class="form-group">
+                    <label class="control-label">Subject</label>
+                    <input type="text" class="form-control" name="subject" placeholder="Subject"
+                           value="{{ old('subject') }}">
+                </div>
+
+                <!-- Message Form Input -->
+                <div class="form-group">
+                    <label class="control-label">Message</label>
+                    <textarea name="message" class="form-control">{{ old('message') }}</textarea>
+                </div>
+
+                @if($users->count() > 0)
+                    <div class="checkbox">
+                        @foreach($users as $user)
+                            <label title="{{ $user->name }}"><input type="checkbox" name="recipients[]"
+                                                                    value="{{ $user->id }}">{!!$user->name!!}</label>
+                        @endforeach
+                    </div>
+                @endif
+
+                <!-- Submit Form Input -->
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary form-control">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
 @stop
--- resources/views/messenger/index.blade.php
+++ resources/views/messenger/index.blade.php
@@ -3,7 +3,13 @@
 @section('pageTitle', 'Messages')
 
 @section('content')
-    @include('messenger.partials.flash')
+    <div class="container-fluid messages-wrapper">
+
+        <div class="row">
+            @include('layouts.page-header', ['header' => 'Messages', 'col' => 6])
+        </div>
+
+        <div class="clearfix"></div>
 
         <div class="row">
             <div class="col-md-12 table-referral-wrapper table-wrapper">
@@ -17,8 +23,16 @@
                     </thead> 
                     <tbody>
                     	@each('messenger.partials.thread', $threads, 'thread', 'messenger.partials.no-threads')
-                    </tbody>   						 
+                    </tbody>		
+                    <tfoot>
+                    	<tr>
+                    		<td colspan="3" class="">
+                    			<a href="/messages/create">New Message</a>
+                    		</td>
+                    	</tr>
+                    </tfoot>	 
     			</table>
     		</div>
     	</div>
+    </div>
 @stop
