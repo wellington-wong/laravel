3007756e353370ab027017c42d235b3e792fc5bc
wellington <wellington@exults.com>
2017-06-16 19:08:33 +0800

    #237118: Incentful - referral program - Setup email when receiving new message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Session;
+use App\Notifications\MessageReceived;
 
 class MessagesController extends Controller
 {
@@ -104,7 +105,12 @@ class MessagesController extends Controller
         // Recipients
         if (Input::has('recipients')) {
             $thread->addParticipant($input['recipients']);
+            foreach ($input['recipients'] as $recipient) {
+                $notifyUser = User::find($recipient);
+                $notifyUser->notify(new MessageReceived($notifyUser));
+            }
         }
+        
         return redirect('messages');
     }
     /**
