473b0375726ac6ff1d8dff2d82aa5249697e0026
wellington <wellington@exults.com>
2017-06-17 02:58:56 +0800

    #237118: Incentful - referral program - Apply uniform style

--- resources/views/manage-account/index.blade.php
+++ resources/views/manage-account/index.blade.php
@@ -4,13 +4,25 @@
 
 @section('content')
 
-    <div class="container-fluid account-profile-wrapper">
-
-        @include('layouts.page-header', ['header' => auth()->user()->display_name, 'col' => 12])
+    <div class="container-fluid manage-account-wrapper">
 
         <div class="row">
-            <div class="col-md-12">
-    	        {{ Form::open(['route'=>'post-account-update', 'id' => 'update-user-form', 'enctype' => 'multipart/form-data']) }}
+        @include('layouts.page-header', ['header' => auth()->user()->display_name, 'col' => 12])
+        </div>
+
+        @if( !$errors->isEmpty() )
+            <div class="alert alert-warning col-md-12">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="row">
+            <div class="col-md-12 no-padding-lr">
+    	        {{ Form::open(['route'=>'post-account-update', 'enctype' => 'multipart/form-data', 'id' => 'update-user-form', 'class' => 'update-form']) }}
 
     	        <div class="col-md-6">
     	        	{{ Form::text('name', auth()->user()->name, ['placeholder' => 'Name', 'class' => 'form-control' . ($errors->has('name') ? ' has-error' : '')]) }}
@@ -26,6 +38,13 @@
 
     	        @include('forms.address', ['city' => null, 'address' => null, 'address2' => null, 'zip' => null, 'state' => null, 'address_placeholder' => 'Address'])
 
+                <div class="form-group col-md-6">
+                    <label for="profile-upload" class="btn btn-default upload-label">{{ old('profile_blob_name') ? 'Filename: ' . old('profile_blob_name') : 'Upload Profile Image' }}</label>
+                    {{ Form::file('profile', array('class' => 'profile-upload hidden', 'id' => 'profile-upload', 'multiple' => 'multiple')) }}
+                </div>    
+
+                {{ Form::hidden('profile_blob', old('profile_blob'), ['class' => 'profile-blob']) }}
+                {{ Form::hidden('profile_blob_name', old('profile_blob_name'), ['class' => 'profile-blob-name']) }}
 
     	        <div class="form-group col-md-12 text-right">
     	        	{{ Form::submit('Update', ['class' => 'btn btn-primary button-responsive-100']) }}
