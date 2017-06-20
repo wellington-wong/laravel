02f651f63fdb588e0b36744f954777b1949b0d47
wellington <wellington@exults.com>
2017-06-21 03:44:00 +0800

    #237118: Incentful - referral program - Update guest navbar links

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -11,11 +11,11 @@
                         <figure class="logo col-xs-4 col-md-3"><a href="{{ route('login') }}" title="Referrals"><img src="{{ isset($_company->logo) ? $_company->logo : '/images/logo.png' }}" alt=""></a></figure>
                         <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
                             <ul class="nav navbar-nav navbar-right navbar-bottom-right">
-                                <li><a href="how-it-works">How it Works</a></li>
-                                <li><a href="features">Features</a></li>
-                                <li><a href="about-us">About Us</a></li>
-                                <li><a href="pricing">Pricing</a></li>
-                                <li><a href="contact">Contact</a></li>
+                                <li><a href="{{ route('how-it-works') }}">How it Works</a></li>
+                                <li><a href="{{ route('features') }}">Features</a></li>
+                                <li><a href="{{ route('about-us') }}">About Us</a></li>
+                                <li><a href="{{ route('pricing') }}">Pricing</a></li>
+                                <li><a href="{{ route('contact') }}">Contact</a></li>
                             </ul>
                         </div>
                     </nav>
