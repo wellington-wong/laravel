e4954c5f2ee0b40ba50d444aa94dbe5e02ac5420
wellington <wellington@exults.com>
2017-06-20 21:50:21 +0800

    #237118: Incentful - referral program - Use company logo on company register page

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <div class="panel panel-default fill">
-                    <div class="panel-heading"><a class="logo" href="{!! url('/') !!}"></a></div>
+                    <div class="panel-heading"><a class="logo" style="background-image: url('/{{ isset($_company->logo) ? $_company->logo : 'images/logo.png' }}')" href="{!! url('/') !!}"></a></div>
                     <div id="app-sidebar-collapse" class=" collapse navbar-collapse">
                         <div class="panel-body">
                             <!-- 
