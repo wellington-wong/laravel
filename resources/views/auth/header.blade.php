3574ecd291b880201a58142f6817a718dbd84a50
wellington <wellington@exults.com>
2017-06-20 21:20:56 +0800

    #237118: Incentful - referral program - Use company logo on company login page

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -8,7 +8,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <figure class="logo col-xs-4 col-md-3"><a href="{{ route('login') }}" title="Referrals"><img src="/images/logo.png" alt=""></a></figure>
+                        <figure class="logo col-xs-4 col-md-3"><a href="{{ route('login') }}" title="Referrals"><img src="{{ isset($_company->logo) ? $_company->logo : '/images/logo.png' }}" alt=""></a></figure>
                         <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
                             <ul class="nav navbar-nav navbar-right navbar-bottom-right">
                                 <li><a href="how-it-works">How it Works</a></li>
