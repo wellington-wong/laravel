f9f2bcb17b830e5f5664192deabc603ba1a17d2d
wellington <wellington@exults.com>
2017-06-21 00:29:10 +0800

    #237118: Incentful - referral program - Update manage account controller

--- app/Http/Controllers/ManageAccountController.php
+++ app/Http/Controllers/ManageAccountController.php
@@ -35,6 +35,27 @@ class ManageAccountController extends Controller
      */
     public function postUpdate( Request $request )
     {
+
+        $rules = [
+            'first_name'=>'required',
+            'last_name'=>'required',
+            'email'=>'required|email',
+            'phone'=>'required|phone:US',
+            'address'=>'max:100',
+            'address2'=>'max:25',
+            'city'=>'required',
+            'state'=>'required|alpha|max:2',
+            'zip'=>'required|max:11',
+            'subdomain_id'=>'required',
+            'install_complete'=>'required'
+        ];
+        $validator = Validator::make($request->input(), $rules);
+
+        if ( $validator->fails() ) {
+            return redirect()->back()->withInput()
+                ->with(['errors'=>$validator->errors()]);
+        }
+
         $phone = new Phone();
         //$phone = auth()->user()->updateDefaultPhone($request);
         auth()->user()->updateProfile($request);
