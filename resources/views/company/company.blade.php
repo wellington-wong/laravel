35f5a59204acff58d46cb5fdf4f468be6c4ada36
dan <daniel.ahern@exults.com>
2017-06-21 13:58:58 +0900

    company info box - DRY

--- resources/views/company/company.blade.php
+++ resources/views/company/company.blade.php
@@ -58,24 +58,7 @@
 
             <div class="col-md-4 company-profile-right">
                 <div class="company-info text-center">            
-                    <div class="company-logo">
-                        <img alt="{{ $_company->company_name }}" src="/{{ isset($_company->logo) ? $_company->logo : 'images/company-placeholder.png' }}" class="img-responsive col-xs-10 col-xs-offset-1"> 
-                        <div class="logo-pencil ajax-logo"><i class="fa fa-pencil"></i></div>
-                        {{ Form::open(['route' => ['post-company-update-logo', $_company->id], 'id' => 'company-update-logo', 'enctype' => 'multipart/form-data']) }}
-                        {{ Form::file('update-logo', ['class' => 'hidden logo-input']) }}
-                        {{ Form::close() }}
-                        <div class="hidden processing">Processing...</div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="company-info-name">{{ $_company->company_name }}</div>
-                    <div class="membership-role">
-                        <h4><strong>Membership Role</strong></h4>
-                        <span>{{ $user->roles->first()->display_name  }}</span>
-                    </div>
-                    <div class="program-url">
-                        <h5>Program URL</h5>
-                        <span><a href="https://{{ $shareUrl }}">{{ $shareUrl }}</a></span>
-                    </div>
+                    @include('company.info_inner')
                 </div>
             </div>
         </div>
