30490b8889528789e828246323efb949d3b9d8a8
wellington <wellington@exults.com>
2017-06-22 01:41:12 +0800

    #237118: Incentful - referral program - Fix ReferralTest error

--- tests/Browser/ReferralsTest.php
+++ tests/Browser/ReferralsTest.php
@@ -6,6 +6,7 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\User;
+use App\Company;
 use Carbon\Carbon;
 
 class ReferralsTest extends DuskTestCase
@@ -22,6 +23,9 @@ class ReferralsTest extends DuskTestCase
         $column = ['created_at', 'id', 'user_id', 'referred', 'status'];
         $search = ['zach', 'daniel', 'bilal', 'wellington'];
         $date = Carbon::create(2017, 1, 0);
+        $subdomain = Company::inRandomOrder()->first()->subdomain;
+        Browser::$baseUrl = 'https://'. $subdomain . '.' . env('DOMAIN');
+
         $parameters = [
             'status' => rand(1, 4),
             'sort' => $sort[array_rand($sort)],
@@ -33,7 +37,7 @@ class ReferralsTest extends DuskTestCase
         // Perform browser test
         $this->browse(function (Browser $browser) use($parameters) {
             $browser->loginAs(User::inRandomOrder()->first()->id)
-                ->visit('/referrals?' . http_build_query($parameters))
+                ->visit( '/referrals?' . http_build_query($parameters))
                 ->waitForText('Referrals')
                 ->assertSee('Referrals');
         });
