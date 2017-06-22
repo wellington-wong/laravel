8c037a0161680ab344c2509741e4446e6797b883
wellington <wellington@exults.com>
2017-06-22 19:00:24 +0800

    #237118: Incentful - referral program - Add profile image to messsaging

--- resources/views/messenger/partials/messages.blade.php
+++ resources/views/messenger/partials/messages.blade.php
@@ -9,13 +9,25 @@
                                     </div>
                                 </div>    
                                 <div class="col-md-1 no-padding-lr avatar text-center">
+                                    @if (isset($message->user->profile_image))
+                                    <div class="profile-image">
+                                        <img height="20" src="/{{ $message->user->profile_image }}" alt="{{ $message->user->name }}" />
+                                    </div>
+                                    @else
                                     <i class="fa fa-user-circle"></i>
+                                    @endif
                                 </div>
                             </td>
                             @else
                             <td>        
                                 <div class="col-md-1 no-padding-lr avatar text-center">
+                                    @if (isset($message->user->profile_image))
+                                    <div class="profile-image">
+                                        <img height="20" src="/{{ $message->user->profile_image }}" alt="{{ $message->user->name }}" />
+                                    </div>
+                                    @else
                                     <i class="fa fa-user-circle"></i>
+                                    @endif
                                 </div>
                                 <div class="col-md-11">
                                     <strong>{{ $message->user->name }}</strong>
