21e76c5092dfadabeb4e66418d139aa256f6eefe
wellington <wellington@exults.com>
2017-06-27 02:10:34 +0800

    #237118: Incentful - referral program - Create generated referral form table

---
+++ database/migrations/2017_06_26_180057_create_generated_referral_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGeneratedReferralForm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('referral_form', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->nullable()->unsigned();
+            $table->string('field');
+            $table->string('value');
+            $table->string('default_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('referral_form');
+    }
+}
