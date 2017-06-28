f3b64fe089383f2044606b5f71a33a9a6e8b53c6
wellington <wellington@exults.com>
2017-06-28 18:23:08 +0800

    #237118: Incentful - referral program - Multi step registration dynamic title

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -453,6 +453,14 @@ $(function (){
 
 // JQUERY STEPS
 	var form = $("#register-form-multistep");
+	var multiStepRegistration = [
+		'We need some basic information about you to get started.',
+		'Tell Us About Your Company',
+		'What information do you need to follow up with a referral?<span class="subtext">This is the information your referral club members will enter when submitting a referral.</span>',
+		'How will you reward your members for their qualifying referrals?',
+		'Please review the information you have entered.'
+	];
+	form.closest('.register-main').find('.top-content').text(multiStepRegistration[0]);
 	/*form.validate({
 		errorPlacement: function errorPlacement(error, element) { element.after(error); },
 		rules: {
@@ -493,6 +501,7 @@ $(function (){
 		  		$('#register-form-multistep #steps-uid-0-p-2').html($('.form-generator'));
 		  		setFormGen = 1;
 			}
+			form.closest('.register-main').find('.top-content').html(multiStepRegistration[newIndex]);
 			return true;//form.valid();
 		},
 		onFinishing: function (event, currentIndex)
