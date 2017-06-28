ceedbbd68d21267bf2d063ddc2b102ea1dba8e30
wellington <wellington@exults.com>
2017-06-28 19:20:47 +0800

    #237118: Incentful - referral program - Add form generator on referral program settings

--- resources/views/program-options/referral-program-settings.blade.php
+++ resources/views/program-options/referral-program-settings.blade.php
@@ -7,6 +7,19 @@
         <div class="row">
         @include('layouts.page-header', ['header' => 'Referral Program Setings', 'col' => 12])
         </div>
+
+        <div class="clearfix"></div>
+
+        <div class="row">
+            <div class="col-md-12 table-referral-settings-wrapper table-wrapper">
+                <div class="form-generator-wrapper">
+                    <div class="form-generator">
+                        <div id="stage1" class="build-wrap"></div>
+                        <form class="render-wrap"></form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
