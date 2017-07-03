70f796219f4e2f4ba532305ee8caff3a73ff06f3
wellington <wellington@exults.com>
2017-07-03 18:09:21 +0800

    #237118: Incentful - referral program - Create referral_submissions table

---
+++ database/migrations/2017_07_03_095940_create_referral_submission.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReferralSubmission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('referral_submissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('referrer_id');
+            $table->integer('subdomain_id');
+            $table->integer('user_id');
+            $table->integer('as_admin_id');
+            $table->integer('status');
+            $table->string('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('referral_submissions');
+    }
+}
