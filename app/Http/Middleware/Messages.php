55dcd055ef80d50450cd845e3dc51cc5b0461d7f
wellington <wellington@exults.com>
2017-07-04 19:27:31 +0800

    #237118: Incentful - referral program - Fix unread message exception on login view

--- app/Http/Middleware/Messages.php
+++ app/Http/Middleware/Messages.php
@@ -19,12 +19,14 @@ class Messages
         // Count all unread message/s for user in current company
         $userThreadsByCompany = $request->_company->threads()->get();
         $messageCount = 0;
-        foreach ($userThreadsByCompany as $key => $thread) {
-            $messageCount = $messageCount + $thread->userUnreadMessagesCount(auth()->user()->id);
+        if (auth()->user()) {
+            foreach ($userThreadsByCompany as $key => $thread) {
+                $messageCount = $messageCount + $thread->userUnreadMessagesCount(auth()->user()->id);
+            }
+            if (auth()->check() && $request->_company->subdomain != 'app') {
+                \Session::put('messageCount', $messageCount);
+            }    
         }
-        if (auth()->check() && $request->_company->subdomain != 'app') {
-            \Session::put('messageCount', $messageCount);
-        }    
 
 
         return $next($request);
