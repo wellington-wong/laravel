8e06b150a8e2ebd58c5c900001eb104bcd3f9e96
wellington <wellington@exults.com>
2017-07-07 05:41:56 +0800

    #237118: Incentful - referral program - Update notificaition emails, add textarea and ifram for rendering html

--- resources/views/program-options/notification-emails.blade.php
+++ resources/views/program-options/notification-emails.blade.php
@@ -9,4 +9,20 @@
         </div>
     </div>
 
+    <div class="clearfix"></div>
+
+    <div class="row">    	
+        <div>
+        <iframe src="/contact/lob/postcard/" id="postcard_iframe">
+        </iframe>
+        </div>
+
+        <label>Postcard HTML</label>
+        <span id="renderHtml" class="btn btn-link" >Render HTML</span>
+        <span id="defaultHtml" class="btn btn-link" >Change to Example HTML (over-writes but doesn't save current HTML)</span>
+        <div>
+        <textarea name="postcard_html" id="postcard_html"></textarea>
+        </div>
+    </div>
+
 @endsection
