2ac3ab149167fbde4044afddac128b38bb17954f
wellington <wellington@exults.com>
2017-07-07 19:41:47 +0800

    #237118: Incentful - referral program - Update EmailTemplate model

--- app/EmailTemplate.php
+++ app/EmailTemplate.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailTemplate extends Model
 {
+
     protected $table = 'email_templates';
+
+    protected $fillable = ['user_id', 'company_id', 'email_html'];
+
+    public function owner() {
+    	return $this->hasOne(User::class);
+    }
+
 }
