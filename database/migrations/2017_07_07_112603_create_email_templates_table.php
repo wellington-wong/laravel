262fe1b873e6fb54235371ee1ab19e2355cc2c5e
wellington <wellington@exults.com>
2017-07-07 19:29:03 +0800

    #237118: Incentful - referral program - Create EmailTemplate model and migration

---
+++ database/migrations/2017_07_07_112603_create_email_templates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->text('email_html');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_templates');
+    }
+}
