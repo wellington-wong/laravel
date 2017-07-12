aa1c4058f416d764851927816ff3257a50de1e37
wellington <wellington@exults.com>
2017-07-13 04:17:11 +0800

    #237118: Incentful - referral program - Fix error exception for missing class

--- resources/views/layouts/page-header.blade.php
+++ resources/views/layouts/page-header.blade.php
@@ -1,5 +1,5 @@
 
-        <div class="page-header col-md-{{ $col }} {{ $class }}">
+        <div class="page-header col-md-{{ $col }}{{ isset($class) ? ' ' . $class : ' ' }}">
             <div class="header-title">
                 <h4>{{ $header }}</h4>
             </div>
