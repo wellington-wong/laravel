971c4bbc01950cdb892ad675ae5bbf76d321df10
wellington <wellington@exults.com>
2017-07-14 20:42:09 +0800

    #237118: Perxi - Add update default phone and address to respective traits

--- app/Traits/Address.php
+++ app/Traits/Address.php
@@ -6,4 +6,25 @@ use User;
 
 trait Address {
 
+    /*
+     * update default address to a user from a request
+     */
+    public function updateDefaultAddress() {
+
+        $request = request();
+        if ( null == $request->input('address')) {
+            return null;
+        }
+
+        $input = [];
+        $address = new Address();
+        foreach ($address->getFillable() as $c) {
+            $input[] = $c;
+        }
+        $address = $this->address()->first()->update(
+            $request->only($input)
+        );
+        $this->address()->updateExistingPivot($this->address()->first()->id, ['default'=>1]);
+        return $address;
+    }
 }
