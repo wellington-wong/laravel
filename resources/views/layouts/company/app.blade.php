5f8dd9c57eafb6b60328b06cc9d05000643c01c1
wellington <wellington@exults.com>
2017-07-19 06:10:16 +0800

    #237118: Perxi - Update css for company custom footer

--- resources/views/layouts/company/app.blade.php
+++ resources/views/layouts/company/app.blade.php
@@ -75,6 +75,11 @@
                             @yield('content')
                         </div>
                     </div>
+                    <footer class="footer">
+                      <div class="container">
+                        <p class="text-muted">Place sticky footer content here.</p>
+                      </div>
+                    </footer>
                 </div>
             </div>
         </div>
