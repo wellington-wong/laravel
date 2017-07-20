1c48b51b3f3adced1c1ea12e4ae570825f6d3cd2
wellington <wellington@exults.com>
2017-07-21 01:04:35 +0800

    #237118: Perxi - Update css for custom company login

--- resources/views/layouts/company/app.blade.php
+++ resources/views/layouts/company/app.blade.php
@@ -46,7 +46,7 @@
                                         <!-- Right Side Of Navbar -->
                                         <ul class="nav navbar-nav navbar-right">
                                             <!-- Authentication Links -->
-                                            <li><a href="{{ route('company-register') }}">Register</a></li>
+                                            <li><a class="action" href="{{ route('company-register') }}">Register</a></li>
                                         </ul>
                                     </div>
                                 </div>
