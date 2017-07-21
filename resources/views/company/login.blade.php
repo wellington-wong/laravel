24ca73adcf17cd5b9c6dad00b59746982a9e9575
wellington <wellington@exults.com>
2017-07-21 10:02:30 +0800

    #237118: Perxi - Update company login view

--- resources/views/company/login.blade.php
+++ resources/views/company/login.blade.php
@@ -54,7 +54,7 @@
 			        </div>
 
 			        <div class="form-group">
-			            <div class="col-md-8 col-md-offset-4">
+			            <div class="col-md-12">
 			                <button type="submit" class="btn btn-primary">
 			                    Login
 			                </button>
