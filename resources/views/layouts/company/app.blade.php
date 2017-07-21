afa0359deab1580a98f990eb5c96b9f153ca873f
wellington <wellington@exults.com>
2017-07-21 23:06:09 +0800

    #237118: Perxi - Add sticky footer to custom company html

--- resources/views/layouts/company/app.blade.php
+++ resources/views/layouts/company/app.blade.php
@@ -75,13 +75,13 @@
                     </div>
                 </div>
             </div>
-        <footer class="footer">
-          <div class="container">
-            <p class="text-muted">Place sticky footer content here.</p>
-          </div>
-        </footer>
         </div>
 
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>
 
     <!-- Scripts -->
 
