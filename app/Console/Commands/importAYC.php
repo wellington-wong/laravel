22b9f58c41413b9c1af05917b7d14329bc1ad9dc
dan <daniel.ahern@exults.com>
2017-07-26 00:57:32 +0900

    import AYC

--- app/Console/Commands/importAYC.php
+++ app/Console/Commands/importAYC.php
@@ -2,7 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Company;
+use App\Lob;
+use App\Referral;
+use App\Role;
+use App\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class importAYC extends Command
 {
@@ -30,6 +36,20 @@ class importAYC extends Command
         parent::__construct();
     }
 
+    /*
+    const STATUS_SUBMITTED      = 1;
+    const STATUS_APPROVED       = 2;
+    const STATUS_REWARD_SENT    = 3;
+    const STATUS_DENIED         = 4;
+     */
+    public static $stati = [
+        'Reward Sent' => Referral::STATUS_REWARD_SENT ,
+        'Claimed' => Referral::STATUS_REWARD_SENT ,
+        'Denied' => Referral::STATUS_DENIED ,
+        'New' => Referral::STATUS_SUBMITTED ,
+        'Verified' => Referral::STATUS_APPROVED ,
+    ];
+
     /**
      * Execute the console command.
      *
@@ -37,6 +57,127 @@ class importAYC extends Command
      */
     public function handle()
     {
-        dd('yup');
+        $ayc_users = DB::table('rewards_users')->get();
+
+        $c = Company::where('subdomain', 'ayc')->first();
+
+        $request = request();
+        $request->merge([''=>'']);
+
+        $userIdByRewardsId = [];
+
+        foreach ($ayc_users as $au) {
+
+            if ( !User::where('email', $au->email)->exists() && $au->deleted != 1 ) {
+
+                $u = new User();
+                $u->email = $au->email;
+                $u->first_name = $au->firstname;
+                $u->last_name = $au->lastname;
+                $u->created_at = $au->created_at;
+                $u->updated_at = $au->updated_at;
+                $u->save();
+
+                //ADD PHONE
+                $request->merge(['phone'=>$au->phone_number]);
+                $u->addDefaultPhone();
+
+                //ADD ADDRESS
+                //CLEANING UP DATA
+                $state = $au->state;
+                if ( array_key_exists($state, Lob::$states ) ) {
+                    $state = Lob::$states[$state];
+                }
+                if( strlen($state) > 2 ) {
+                    $state = 'NA';
+                }
+                $zip = $au->zip;
+                if ( strlen($zip) > 11 ) {
+                    $zip = substr($zip, 0, 10);
+                }
+                $request->merge(['address'=>$au->address]);
+                $request->merge(['city'=>$au->city]);
+                $request->merge(['state'=>$state]);
+                $request->merge(['zip'=>$zip]);
+                $u->addDefaultAddress();
+
+                //ADD ROLE
+                if ( 2 == $au->usertype ) {
+                    $role = 'superAdmin';
+                } elseif ( 1 == $au->usertype ) {
+                    $role = 'admin';
+                } else {
+                    $role = 'member';
+                }
+                $role = Role::where('name', $role)->first();
+                $u->attachRole($role, $c);
+
+                //dd($u);
+
+            } else {
+                $u = User::where('email', $au->email)->first();
+            }
+
+            if ( isset($u) )
+            $userIdByRewardsId[$au->id] = $u->id;
+
+        }
+
+        $ayc_referrals = DB::table('rewards_referrals')->get();
+
+        foreach ( $ayc_referrals as $au ) {
+
+            if ( !User::where('email', $au->email)->exists() && $au->deleted != 1 && '' != $au->first_name ) {
+
+                $u = new User();
+                $u->email = $au->email;
+                $u->first_name = $au->first_name;
+                $u->last_name = $au->last_name;
+                $u->created_at = $au->created_at;
+                $u->updated_at = $au->updated_at;
+                $u->save();
+
+                //ADD PHONE
+                $request->merge(['phone'=>$au->phone_number]);
+                $u->addDefaultPhone();
+
+                //ADD ADDRESS
+                //CLEANING UP DATA
+                $state = $au->state;
+                if ( array_key_exists($state, Lob::$states ) ) {
+                    $state = Lob::$states[$state];
+                }
+                if( strlen($state) > 2 ) {
+                    $state = 'NA';
+                }
+                $zip = $au->zip;
+                if ( strlen($zip) > 11 ) {
+                    $zip = substr($zip, 0, 10);
+                }
+                $request->merge(['address'=>$au->address]);
+                $request->merge(['city'=>$au->city]);
+                $request->merge(['state'=>$state]);
+                $request->merge(['zip'=>$zip]);
+                $u->addDefaultAddress();
+
+                //ADD ROLE
+                $role = 'member';
+                $role = Role::where('name', $role)->first();
+                $u->attachRole($role, $c);
+
+                $ref = new Referral();
+                $ref->referrer_id   = $userIdByRewardsId[$au->user_id];
+                $ref->company_id    = $c->id;
+                $ref->user_id       = $u->id;
+                $ref->status        = static::$stati[$au->status];
+                $ref->created_at    = $au->created_at;
+                $ref->updated_at    = $au->updated_at;
+                $ref->save();
+
+            }
+
+            //dd($au);
+        }
+        //dd($ayc_referrals);
     }
 }
--- app/Traits/AddressTrait.php
+++ app/Traits/AddressTrait.php
@@ -6,6 +6,27 @@ use App\Address;
 
 trait AddressTrait {
 
+
+    public function addDefaultAddress() {
+
+        $request = request();
+        if ( null == $request->input('address')) {
+            return null;
+        }
+        $input = [];
+        $address = new Address();
+        foreach ($address->getFillable() as $c) {
+            if ( isset($request->$c) ) {
+                $input[] = $c;
+            }
+        }
+        $address = $this->address()->create(
+            $request->only($input)
+        );
+        $this->address()->updateExistingPivot($address->id, ['default'=>1]);
+        return $address;
+    }
+
     /*
      * update default address to a user from a request
      */
@@ -21,9 +42,10 @@ trait AddressTrait {
         foreach ($address->getFillable() as $c) {
             $input[] = $c;
         }
-        if (null === $this->address()->first()) { return; }
+        //THIS REMOVES LOB VERIFICATION INFO FOR THE OLD ADDRESS
         $address = $this->address()->first()->update(
-            $request->only($input)
+            array_merge($request->only($input), ['lob_verified'=>'0', 'lob_response'=>null])
+        //['lob_verified'=>'0', 'lob_response'=>null] + $request->only($input)
         );
         $this->address()->updateExistingPivot($this->address()->first()->id, ['default'=>1]);
         return $address;
