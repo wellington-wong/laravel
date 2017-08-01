622e643dcea336b23d2ae052da9434baf85c6a31
wellington <wellington@exults.com>
2017-08-02 02:46:19 +0800

    #237118: Perxi - Update notification files

--- app/Notifications/NewMemberAdmin.php
+++ app/Notifications/NewMemberAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class NewMessage extends Notification
+class NewMemberAdmin extends Notification
 {
     use Queueable;
 
