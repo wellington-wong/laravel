06e0f2e8a0fc340d5af9bbef8cfdd41673e83500
wellington <wellington@exults.com>
2017-08-01 19:55:20 +0800

    #237118: Perxi - Update js links to app.exnf.com

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,10 +81,10 @@
     {{--<script src="https://app.{{ env('DOMAIN') }}/js/manifest.js"></script>
     <script src="https://app.{{ env('DOMAIN') }}/js/vendor.js"></script>
     <script src="https://app.{{ env('DOMAIN') }}/js/app.js"></script>--}}
-    <script src="{{ mix('/js/manifest.js') }}"></script>
-    <script src="{{ mix('/js/vendor.js') }}"></script>
-    <script src="{{ mix('/js/app.js') }}"></script>
-    <script src="{{ mix('/js/all.js') }}"></script>
+    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/manifest.js') }}"></script>
+    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/vendor.js') }}"></script>
+    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/app.js') }}"></script>
+    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/all.js') }}"></script>
 
     @yield('js')
 
