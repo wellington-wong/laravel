4b1aa9b029a4951350a01a379afb302b0b36fdf7
wellington <wellington@exults.com>
2017-08-02 20:01:59 +0800

    #237118: Perxi - Fix css and js for app.exnf.com login page

--- resources/views/auth/document-top.blade.php
+++ resources/views/auth/document-top.blade.php
@@ -16,9 +16,10 @@
     <title>@yield('pageTitle', '') | {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+    <link href="https://app.{{ env('DOMAIN') }}{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="https://app.{{ env('DOMAIN') }}{{ mix('css/all.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
