74a8e28d9387819b4637ca23911f35596a97602d
wellington <wellington@exults.com>
2017-08-02 18:07:31 +0800

    #237118: Perxi - Update referral received notification

--- app/Notifications/ReferralReceived.php
+++ app/Notifications/ReferralReceived.php
@@ -16,9 +16,11 @@ class ReferralReceived extends Notification
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($referral, $request, $referralValues)
     {
-        //
+        $this->referral = $referral;
+        $this->referralValues = $referralValues;
+        $this->request = $request;
     }
 
     /**
@@ -29,7 +31,7 @@ class ReferralReceived extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -40,10 +42,64 @@ class ReferralReceived extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        if ($emailHtml = $this->request->_company->emailTemplate()->first()) {
+
+            $regex = '#{{(.*?)}}#';
+            $code = preg_match_all($regex, $emailHtml, $matches);
+
+            // Get referred vars for referral
+            $replacementVars = [];
+            foreach ($matches[1] as $match) {
+                if (stristr($match, 'referrer')) {
+                    $varName = str_replace('referrer_', '', trim($match));
+                    if (trim($match) == 'referrer_phone') {
+                        $replacementVars[trim($match)] = isset($this->referral->referrer->phone[0]->phone) ? $this->referral->referrer->phone[0]->phone : null;
+                    } else if (trim($match) == 'referrer_address') {
+                        $replacementVars[trim($match)] = isset($this->referral->referrer->address[0]->address) ? $this->referral->referrer->address[0]->address : null;
+                    } else {
+                        $replacementVars[trim($match)] = $this->referral->referrer->$varName;
+                    }
+                }                
+            }
+
+            // Get referrer vars for referral
+            foreach ($this->referralValues as $referralValue) {
+                foreach ($matches[1] as $match) {
+                    if (stristr($match, 'referred')) {
+                        $varName = str_replace('referred_', '', trim($match));
+                        if (trim($match) == 'referred_phone') {
+                            $replacementVars[trim($match)] = isset($this->referral->referred->phone[0]->phone) ? $this->referral->referred->phone[0]->phone : null;
+                        } else if (trim($match) == 'referred_address') {
+                            $replacementVars[trim($match)] = isset($this->referral->referred->address[0]->address) ? $this->referral->referred->address[0]->address : null;
+                        } else {
+                            $replacementVars[trim($match)] = $this->referral->referred->$varName;
+                        }
+                    }                
+                }
+            }
+
+            // Replace placeholder with real user data
+            foreach ($replacementVars as $key => $replacementVar) {
+                $emailHtml->email_html = str_replace('{{ ' . $key . ' }}', $replacementVars[$key], $emailHtml->email_html);
+            }    
+
+            return (new MailMessage)
+                ->markdown('email-templates.referral-notify-user', ['referral' => $this->referral, 'email_template' => $emailHtml->email_html]);
+        } else {
+            return (new MailMessage)
+                ->line('You have been referred by ' . auth()->user()->getName());
+        }
+    }
+
+    /**
+     * Get the database representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return $this->referral->toArray();
     }
 
     /**
