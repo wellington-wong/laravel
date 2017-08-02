4a36db78043467972042c0fa066d6315caa17181
wellington <wellington@exults.com>
2017-08-02 20:32:15 +0800

    #237118: Perxi - Unify head tag for all blades

--- resources/views/auth/document-top.blade.php
+++ resources/views/auth/document-top.blade.php
@@ -1,31 +1,4 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="env" content="{{ config('app.env') }}" >
-    @if ( 'local' == config('app.env') )
-        <meta name="robots" content="noindex" >
-    @endif
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('pageTitle', '') | {{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="https://app.{{ env('DOMAIN') }}{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="https://app.{{ env('DOMAIN') }}{{ mix('css/all.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
-</head>
+@include('layouts.head')
     <body class="basic-page">
--- resources/views/layouts/company/app.blade.php
+++ resources/views/layouts/company/app.blade.php
@@ -1,40 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}" class="custom-company-html">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="env" content="{{ config('app.env') }}" >
-    @if ( 'local' == config('app.env') )
-        <meta name="robots" content="noindex" >
-    @endif
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>@yield('pageTitle', '') | {{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/all.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Domine" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
-
-    <link href="https://app.{{ env('DOMAIN') }}{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="https://app.{{ env('DOMAIN') }}{{ mix('css/all.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Domine" rel="stylesheet">
-    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
-</head>
+@include('layouts.head')
 <body class="company-custom-body">
         <div class="container-fluid">
             <div class="row">
@@ -93,13 +59,6 @@
     </footer>
 
     <!-- Scripts -->
-
-    {{--<script src="{{ mix('/js/manifest.js') }}"></script>
-    <script src="{{ mix('/js/vendor.js') }}"></script>
-    <script src="{{ mix('/js/app.js') }}"></script>
-    <script src="{{ mix('/js/all.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>--}}
-    
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/manifest.js') }}"></script>
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/vendor.js') }}"></script>
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/app.js') }}"></script>
