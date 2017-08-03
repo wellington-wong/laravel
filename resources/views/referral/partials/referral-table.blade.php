a8f5ac2a520aee1ec396a02b6bcf6e89f0b55281
wellington <wellington@exults.com>
2017-08-04 03:25:15 +0800

    #237118: Perxi - Fix query parameters on pagination

--- resources/views/referral/partials/referral-table.blade.php
+++ resources/views/referral/partials/referral-table.blade.php
@@ -84,7 +84,7 @@
                     @endforeach
                     @if (!count($referrals))<tr><td colspan="5">No referrals found.</td></tr>@endif
                 </table>
-                <div class="col-md-12 pagination-wrapper">{{ $referrals->links() }}</div>
+                <div class="col-md-12 pagination-wrapper">{{ $referrals->appends(app('request')->query())->links() }}</div>
             </div>
         </div>
         @include('layouts.modal')
