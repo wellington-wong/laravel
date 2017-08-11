b874bd72c03e2a009e78175f8f60a2810d8a0ad1
wellington <wellington@exults.com>
2017-08-12 00:15:08 +0800

    #237118: Perxi - Update referral program settings test

--- tests/Browser/ReferralProgramSettingsTest.php
+++ tests/Browser/ReferralProgramSettingsTest.php
@@ -5,6 +5,9 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use App\Role;
+use App\Company;
 
 class ReferralProgramSettingsTest extends DuskTestCase
 {
@@ -15,9 +18,16 @@ class ReferralProgramSettingsTest extends DuskTestCase
      */
     public function testExample()
     {
+
+        // Visit random subdomain
+        $subdomain = Company::inRandomOrder()->first()->subdomain;
+        Browser::$baseUrl = 'https://'. $subdomain . '.' . env('DOMAIN');
+
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser->loginAs(Role::where('name', 'globalAdmin')->first()->users()->first()->id)
+                ->visit('/program-options/referral-program-settings')
+                ->assertSee('Custom Company Landing Page')
+                ->assertSee('Referral Program Settings');
         });
     }
 }
