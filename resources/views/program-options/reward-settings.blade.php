01fb246c2a280afd5d69b672022324b7a77c3f6f
wellington <wellington@exults.com>
2017-08-14 20:08:10 +0800

    #237118: Perxi - Update reward settings blde

--- resources/views/program-options/reward-settings.blade.php
+++ resources/views/program-options/reward-settings.blade.php
@@ -11,12 +11,27 @@
 
     <div class="clearfix"></div>
 
-
     <div class="row">    	
-      {{ Form::open(['route'=>'reward-settings', 'enctype' => 'multipart/form-data', 'id' => 'reward-settings-form', 'class' => 'reward-settings-form']) }}
+      {{ Form::open(['route'=>'program-options-reward-settings', 'enctype' => 'multipart/form-data', 'id' => 'reward-settings-form', 'class' => 'reward-settings-form']) }}
       <div class="col-md-12">
-            <label>Foreground Color</label>
-            <div><input type="text" name="foreground_color" value="{{ isset($_company->foreground_color) ? $_company->foreground_color : '#333333' }}" class="form-control render-spectrum"></div>
+            <label>Reward Title</label>
+            {{ Form::text('reward_title', null, ['placeholder' => 'Reward Title', 'value' => old('reward_title'), 'class' => 'reward-title' . ($errors->has('reward_title') ? ' has-error' : '')]) }}
+      </div>
+      <div class="col-md-12">
+            <label>What kind of reward will you use?</label>
+            {{ Form::select('reward_kind', null, ['placeholder' => 'Reward Kind', 'value' => old('reward_kind'), 'class' => 'reward-kind' . ($errors->has('reward_kind') ? ' has-error' : '')]) }}
+      </div>
+      <div class="col-md-12">
+            <label>How will you send the reward?</label>
+            {{ Form::select('reward_send', null, ['placeholder' => 'Reward Send', 'value' => old('reward_send'), 'class' => 'reward-title' . ($errors->has('reward_send') ? ' has-error' : '')]) }}
+      </div>
+      <div class="col-md-12">
+            <label>Would you like to show a leaderboard on your site? <a href="#">What's this?</a></label>
+            {{ Form::select('leaderboard', null, ['placeholder' => 'Leaderboard', 'value' => old('leaderboard'), 'class' => 'leaderboard' . ($errors->has('leaderboard') ? ' has-error' : '')]) }}
+      </div>
+      <div class="col-md-12">
+            <label>Reward Ratio</label>
+            {{ Form::text('reward_ratio', null, ['placeholder' => 'Reward Ratio', 'value' => old('reward_ratio'), 'class' => 'reward-ratio' . ($errors->has('reward_ratio') ? ' has-error' : '')]) }}
       </div>
       {{ Form::close() }}
     </div>
