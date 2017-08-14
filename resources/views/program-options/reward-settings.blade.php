9f1bacc0eb8d8c90b1b0e7fc134edab8b1140c0d
wellington <wellington@exults.com>
2017-08-14 22:19:06 +0800

    #237118: Perxi - Update reward settings blde

--- resources/views/program-options/reward-settings.blade.php
+++ resources/views/program-options/reward-settings.blade.php
@@ -13,25 +13,35 @@
 
     <div class="row">    	
       {{ Form::open(['route'=>'program-options-reward-settings', 'enctype' => 'multipart/form-data', 'id' => 'reward-settings-form', 'class' => 'reward-settings-form']) }}
-      <div class="col-md-12">
+      <div class="col-md-6">
             <label>Reward Title</label>
-            {{ Form::text('reward_title', null, ['placeholder' => 'Reward Title', 'value' => old('reward_title'), 'class' => 'reward-title' . ($errors->has('reward_title') ? ' has-error' : '')]) }}
+            <div class="col-md-12 no-padding-lr">
+            {{ Form::text('reward_title', null, ['placeholder' => 'Reward Title', 'value' => old('reward_title'), 'class' => 'form-control reward-title' . ($errors->has('reward_title') ? ' has-error' : '')]) }}
+            </div>
       </div>
-      <div class="col-md-12">
+      <div class="col-md-6">
             <label>What kind of reward will you use?</label>
-            {{ Form::select('reward_kind', ['placeholder' => 'Reward Kind', 'value' => old('reward_kind'), 'class' => 'reward-kind' . ($errors->has('reward_kind') ? ' has-error' : '')]) }}
+            <div class="col-md-12 no-padding-lr">
+            {{ Form::select('reward_kind', ['placeholder' => 'Reward Kind', 'value' => old('reward_kind'), 'class' => 'form-control reward-kind' . ($errors->has('reward_kind') ? ' has-error' : '')]) }}
+            </div>
       </div>
-      <div class="col-md-12">
+      <div class="col-md-6">
             <label>How will you send the reward?</label>
-            {{ Form::select('reward_send', ['placeholder' => 'Reward Send', 'value' => old('reward_send'), 'class' => 'reward-title' . ($errors->has('reward_send') ? ' has-error' : '')]) }}
+            <div class="col-md-12 no-padding-lr">
+            {{ Form::select('reward_send', ['placeholder' => 'Reward Send', 'value' => old('reward_send'), 'class' => 'form-control reward-title' . ($errors->has('reward_send') ? ' has-error' : '')]) }}
+            </div>
       </div>
-      <div class="col-md-12">
-            <label>Would you like to show a leaderboard on your site? <a href="#">What's this?</a></label>
-            {{ Form::select('leaderboard', ['placeholder' => 'Leaderboard', 'value' => old('leaderboard'), 'class' => 'leaderboard' . ($errors->has('leaderboard') ? ' has-error' : '')]) }}
+      <div class="col-md-6">
+            <label>Would you like to show a leaderboard on your site? <a href="#">What's this?</a></label>            
+            <div class="col-md-12 no-padding-lr">
+            {{ Form::select('leaderboard', ['placeholder' => 'Leaderboard', 'value' => old('leaderboard'), 'class' => 'form-control leaderboard' . ($errors->has('leaderboard') ? ' has-error' : '')]) }}
+            </div>
       </div>
-      <div class="col-md-12">
-            <label>Reward Ratio</label>
-            {{ Form::text('reward_ratio', null, ['placeholder' => 'Reward Ratio', 'value' => old('reward_ratio'), 'class' => 'reward-ratio' . ($errors->has('reward_ratio') ? ' has-error' : '')]) }}
+      <div class="col-md-6">
+            <label>Reward Ratio</label>            
+            <div class="col-md-12 no-padding-lr">
+            {{ Form::text('reward_ratio', null, ['placeholder' => 'Reward Ratio', 'value' => old('reward_ratio'), 'class' => 'form-control reward-ratio' . ($errors->has('reward_ratio') ? ' has-error' : '')]) }}
+            </div>
       </div>
       {{ Form::close() }}
     </div>
