e89bba2446d55addfcb4da2456554bfdc23e0580
wellington <wellington@exults.com>
2017-08-15 17:50:14 +0800

    #237118: Perxi - Update reward setting, add form-control class

--- resources/views/program-options/reward-settings.blade.php
+++ resources/views/program-options/reward-settings.blade.php
@@ -13,31 +13,31 @@
 
     <div class="row">    	
       {{ Form::open(['route'=>'program-options-reward-settings', 'enctype' => 'multipart/form-data', 'id' => 'reward-settings-form', 'class' => 'reward-settings-form']) }}
-      <div class="col-md-6">
+      <div class="col-md-6 form-group">
             <label>Reward Title</label>
             <div class="col-md-12 no-padding-lr">
             {{ Form::text('title', old('title'), ['placeholder' => 'Reward Title', 'class' => 'form-control reward-title' . ($errors->has('title') ? ' has-error' : '')]) }}
             </div>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 form-group">
             <label>What kind of reward will you use?</label>
             <div class="col-md-12 no-padding-lr">
             {{ Form::select('reward_kind', ['placeholder' => 'Reward Kind'],  old('reward_kind'), ['class' => 'form-control reward-kind' . ($errors->has('reward_kind') ? ' has-error' : '')]) }}
             </div>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 form-group">
             <label>How will you send the reward?</label>
             <div class="col-md-12 no-padding-lr">
             {{ Form::select('reward_send', ['placeholder' => 'Reward Send'], old('reward_send'), ['class' => 'form-control reward-title' . ($errors->has('reward_send') ? ' has-error' : '')]) }}
             </div>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 form-group">
             <label>Reward Ratio</label>            
             <div class="col-md-12 no-padding-lr">
             {{ Form::text('reward_ratio', null, ['placeholder' => 'Reward Ratio', 'value' => old('reward_ratio'), 'class' => 'form-control reward-ratio' . ($errors->has('reward_ratio') ? ' has-error' : '')]) }}
             </div>
       </div>
-      <div class="col-md-12">          
+      <div class="col-md-12 form-group">          
             <div class="col-md-12 no-padding-lr">
             	<label>Would you like to show a leaderboard on your site? <a href="#">What's this?</a></label>
             </div>            
@@ -51,7 +51,7 @@
             </div>
       </div>
 
-      <div class="form-group col-md-12 text-right">
+      <div class="form-group col-md-12 text-right form-group">
       	{{ Form::submit('Update', ['class' => 'btn btn-primary button-responsive-100 submit-profile']) }}
       </div>
 
