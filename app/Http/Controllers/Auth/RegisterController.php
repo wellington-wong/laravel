28d79bef63d7d1365939ed758aaff4cd2c31e5f9
wellington <wellington@exults.com>
2017-08-16 21:48:59 +0800

    #237118: Perxi - Add RewardSetting model to RegisterControlelr

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\User;
 use App\Company;
 use App\ReferralForms;
+use App\RewardSetting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
