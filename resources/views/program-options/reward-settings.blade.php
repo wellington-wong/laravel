bdeb8cbf6085169120efcce7acf7bc798d54da74
wellington <wellington@exults.com>
2017-08-19 02:24:50 +0800

    #237118: Perxi - Fix reward settings view, remove 0 in select boxes

--- resources/views/program-options/reward-settings.blade.php
+++ resources/views/program-options/reward-settings.blade.php
@@ -22,13 +22,13 @@
       <div class="col-md-6 form-group">
             <label>What kind of reward will you use?</label>
             <div class="col-md-12 no-padding-lr">
-            {{ Form::select('reward_kind', [\App\RewardSetting::$rewardSend],  $_company->rewardSettings()->first()->reward_kind, ['class' => 'form-control reward-kind' . ($errors->has('reward_kind') ? ' has-error' : '')]) }}
+            {{ Form::select('reward_kind', \App\RewardSetting::$rewardSend,  $_company->rewardSettings()->first()->reward_kind, ['class' => 'form-control reward-kind' . ($errors->has('reward_kind') ? ' has-error' : '')]) }}
             </div>
       </div>
       <div class="col-md-6 form-group">
             <label>How will you send the reward?</label>
             <div class="col-md-12 no-padding-lr">
-            {{ Form::select('reward_send', [\App\RewardSetting::$rewardKind], $_company->rewardSettings()->first()->reward_send, ['class' => 'form-control reward-title' . ($errors->has('reward_send') ? ' has-error' : '')]) }}
+            {{ Form::select('reward_send', \App\RewardSetting::$rewardKind, $_company->rewardSettings()->first()->reward_send, ['class' => 'form-control reward-title' . ($errors->has('reward_send') ? ' has-error' : '')]) }}
             </div>
       </div>
       <div class="col-md-6 form-group">
