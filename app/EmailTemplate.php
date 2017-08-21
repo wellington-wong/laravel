6c934779adc395c8557b1a94d630e5600f5d6e6b
wellington <wellington@exults.com>
2017-08-21 21:44:52 +0800

    #237118: Perxi - Fix error exception when viewing notification emails view

--- app/EmailTemplate.php
+++ app/EmailTemplate.php
@@ -16,7 +16,7 @@ class EmailTemplate extends Model
     const ADMIN_NEW_MEMBER	= 6;
     const ADMIN_NEW_REFERRAL	= 7;
 
-    static $label = [
+    static $labels = [
         self::NEW_MEMBER => 'New Member Welcome Email',
         self::REFERRAL_RECEIVED => 'Referral Received Email',
         self::REFERRAL_VERIFIED => 'Referral Verified Notification Email',
