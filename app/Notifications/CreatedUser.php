33ed0963b308b176bb54aab6d6b89c45f18e8bb8
wellington <wellington@exults.com>
2017-08-23 23:45:20 +0800

    #237118: Perxi - CreatedUser notification, update subject

--- app/Notifications/CreatedUser.php
+++ app/Notifications/CreatedUser.php
@@ -43,9 +43,9 @@ class CreatedUser extends Notification
         return (new MailMessage)
                     ->subject('Activate Your Perxi Account')
                     ->line('You\'re account for Perxi has been successfully created.')
-                    ->line('Please login using the link below.')
+                    ->line('Please login using the link below to set a password.')
                     ->line($this->token)
-                    ->action('Notification Action', url('/'))
+                    ->action('Notification Action', url('/password/reset/' . $this->token . '?set=1'))
                     ->line('Thank you for using our application!');
     }
 
