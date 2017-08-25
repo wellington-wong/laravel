444e15b13d54df19472f1a427b6b5803d7dfb65c
wellington <wellington@exults.com>
2017-08-26 00:29:47 +0800

    #237118 - Perxi: Add referrer name and address to exported referrals xls file

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -200,11 +200,10 @@ class ReferralController extends Controller
             $currentReferral = [
                 'SUBMITTED' => $referral->referred->created_at->format('m/d/y'),
                 'REFERRAL ID' => $referral->id,
-                'SUBMITTED BY' => isset($referral->referrer->name) ? $referral->referrer->name : $referral->referrer->first_name . ' ' . $referral->referrer->last_name,
-                'NAME' => isset($referral->referred->name) ? $referral->referred->name : $referral->referred->first_name . ' ' . $referral->referred->last_name,
-                'EMAIL' => $referral->referred->email,
                 'REFERRER NAME' => $referral->referrer->getName(),
                 'REFERRER ADDRESS' => isset($longAddress) ? ($longAddress->address . ' ' . $longAddress->address2 . ', ' . $longAddress->city . ', ' . $longAddress->state . ', ' . $longAddress->zip) : null,
+                'NAME' => isset($referral->referred->name) ? $referral->referred->name : $referral->referred->first_name . ' ' . $referral->referred->last_name,
+                'EMAIL' => $referral->referred->email,          
                 'STATUS' => \App\Referral::$status[$referral->status]
             ];
             $referralArray[] = $currentReferral;
