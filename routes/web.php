cc57dacd3825556a30b92f958fc561c1e6256704
wellington <wellington@exults.com>
2017-09-13 22:47:31 +0800

    #237118: Perxi - Filter roles that can delete members and admins

--- routes/web.php
+++ routes/web.php
@@ -134,7 +134,7 @@ Route::group(['prefix' => '/', 'middleware' => ['role:admin|superAdmin|globalAdm
 	Route::get('/members', 'MembersController@getIndex')->name('members');
 	//Route::get('/members/{id}', 'MembersController@members')->name('member');
 	Route::get('/member/create', 'MembersController@create')->name('members-create');
-	Route::get('/member/delete/{id}', 'MembersController@delete')->name('members-delete');
+	Route::get('/member/delete/{id}', ['uses' => 'MembersController@delete', 'middleware' => ['role:superAdmin|globalAdmin']])->name('members-delete');
 
 	// Notification Routes
 	Route::get('/notification/{nid}', 'NotificationsController@getNotification')->name('notification');
