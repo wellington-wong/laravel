5b6d1cd6f4dccdfd89bc124ea70648eb39e82b9b
wellington <wellington@exults.com>
2017-09-21 18:42:15 +0800

    #250946: Perxi - critical fixes, Add content text for 'How to Get More Referrals'

--- resources/views/basic/how-to-get-more-referrals.blade.php
+++ resources/views/basic/how-to-get-more-referrals.blade.php
@@ -9,11 +9,16 @@
         </div>
         <div class="row">
             <div class="col-md-12 no-padding-lr">
-                <p>Lorem ipsum dolor sit amet, mea audiam philosophia ne, ex tamquam inimicus eos. Labore contentiones quo ne, quo epicuri voluptua ei. Cum modo inani ex, eu vidit ipsum sadipscing mei. Mel te reque ornatus oportere, ius maiorum mediocrem iudicabit ne. Iriure dolorum complectitur usu cu, primis deseruisse et duo.</p>
-                <p>Pro adhuc aliquip numquam et. Id vim meis vidisse luptatum, vix case fastidii no. Ponderum cotidieque appellantur usu ex, legimus reprehendunt nam ex, at bonorum fabulas definitionem mei. Nec solet fabulas ocurreret te, scripta similique moderatius nec et. In regione detracto qui, et postea nemore labores per. Ad tation ubique sensibus sea, eum laudem eruditi fierent id, quaestio assentior elaboraret eam id. Veri vivendo appetere sea no.</p>
-                <p>Labitur complectitur vel ea. Vis falli oportere ei, assentior consectetuer vel ei. Id sea dictas meliore. Ut eam fabulas periculis. Populo pericula vel ea, nulla doctus deleniti vis ea, sea ut partiendo patrioque iracundia.</p>
-                <p>Vide definitiones ex eam. Pro mazim propriae ex, et sea purto omittantur, pri in stet eligendi mandamus. Te cum eirmod debitis persecuti. Usu odio quando luptatum in. Alienum quaerendum neglegentur an per, odio vide exerci eu ius, fabellas petentium vix et. Vel eu quod essent, no eos aeque sapientem facilisis.</p>
-                <p>Cu rationibus sadipscing eam, ut ius vero aperiam, quo ne nostro labitur consulatu. Cu vim assum ignota similique, eius comprehensam mei ea. Ut decore aeterno aperiri usu, at usu illud persecuti, solet nonumes adversarium ex vel. Cum eu voluptaria persequeris, in mei quem fabulas, est luptatum adipiscing cu. Mel at dolorum admodum, ea malorum omnesque disputando mei, mel eruditi voluptua ne.</p>
+                               
+                <p>Ways to get more referrals:</p>
+                <ul>
+                    <li><p><strong>Word of Mouth.</strong> By spreading the word to your family, friends and coworkers, you won’t believe how many people are looking to install a new AC unit.</p></li>
+                    <li><p><strong>Social Media.</strong> Using our provided images for social media posting, your reach can increase exponentially to people that aren’t in your usual circle of friends.</p></li>
+                    <li><p><strong>Printable Flyer or Postcard.</strong> For those of you who utilize poster boards in the office or flyer around the neighborhood, this referral resource is perfect for you!</p></li>
+                    <li><p><strong>Printable Referral Cards.</strong> In the real estate or construction business? You can keep a referral card with you while working and give your cards to potential clients that need AC installation.</p></li>
+                </ul>
+
+
             </div>
         </div>
     </div>
