affb50c887b465ceb804a94fd420e735587d63ba
wellington <wellington@exults.com>
2017-09-21 22:01:52 +0800

    #250946: Perxi - critical fixes, Update 'Edit Basic Pages' links

--- resources/views/global-settings/edit-basic-pages.blade.php
+++ resources/views/global-settings/edit-basic-pages.blade.php
@@ -17,10 +17,10 @@
             <div class="col-md-12 table-referral-wrapper table-wrapper">
                 <label>Auth Pages</label>
                 <ul>
-                  <li><a href="{{ route('referral-rewards') }}">Rewards</a></li>
-                  <li><a href="{{ route('how-this-works') }}">How This Works</a></li>
-                  <li><a href="{{ route('how-to-get-more-referrals') }}">How to Get More Referrals</a></li>
-                  <li><a href="{{ route('help') }}">Need help?</a></li>
+                  <li><a href="{{ route('referral-rewards', ['edit' => 1]) }}">Rewards</a></li>
+                  <li><a href="{{ route('how-this-works', ['edit' => 1]) }}">How This Works</a></li>
+                  <li><a href="{{ route('how-to-get-more-referrals', ['edit' => 1]) }}">How to Get More Referrals</a></li>
+                  <li><a href="{{ route('help', ['edit' => 1]) }}">Need Help?</a></li>
                 </ul>
             </div>
         </div>
@@ -29,10 +29,10 @@
             <div class="col-md-12 table-referral-wrapper table-wrapper">
                 <label>Guest Pages</label>
                 <ul>
-                  <li><a href="{{ route('how-it-works') }}">How it Works</a></li>
-                  <li><a href="{{ route('features') }}">Features</a></li>
-                  <li><a href="{{ route('about-us') }}">About Us</a></li>
-                  <li><a href="{{ route('pricing') }}">Pricing</a></li>
+                  <li><a href="{{ route('how-it-works', ['edit' => 1]) }}">How it Works</a></li>
+                  <li><a href="{{ route('features', ['edit' => 1]) }}">Features</a></li>
+                  <li><a href="{{ route('about-us', ['edit' => 1]) }}">About Us</a></li>
+                  <li><a href="{{ route('pricing', ['edit' => 1]) }}">Pricing</a></li>
                 </ul>
             </div>
         </div>
