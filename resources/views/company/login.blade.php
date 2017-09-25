22b812d28d7424b48b75af704a7607f1a17515fd
wellington <wellington@exults.com>
2017-09-25 23:36:52 +0800

    #250946: Perxi - critical, Add required asterisk on company login page

--- resources/views/company/login.blade.php
+++ resources/views/company/login.blade.php
@@ -16,7 +16,7 @@
 			        {{ csrf_field() }}
 
 			        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-			            <label for="email" class="col-md-12">EMAIL</label>
+			            <label for="email" class="col-md-12">EMAIL *</label>
 
 			            <div class="col-md-12">
 			                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -30,7 +30,7 @@
 			        </div>
 
 			        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-			            <label for="password" class="col-md-12">PASSWORD</label>
+			            <label for="password" class="col-md-12">PASSWORD *</label>
 
 			            <div class="col-md-12">
 			                <input id="password" type="password" class="form-control" name="password" required>
