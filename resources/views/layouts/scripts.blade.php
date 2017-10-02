0933ce75c0dbd28e6d365e019d4e173be2c02f1f
wellington <wellington@exults.com>
2017-10-03 00:55:26 +0800

    #237118: Perxi - Post-Launch Fixes, Fix company registraion for reward ratio validation

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -3,5 +3,6 @@
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/vendor.js') }}"></script>
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/app.js') }}"></script>
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/all.js') }}"></script>
+	<script src="https://js.stripe.com/v3/"></script>
 
     @yield('js')
