10bd0ebb3315acf22d547c641583156b3e9b33a7
wellington <wellington@exults.com>
2017-10-05 22:38:37 +0800

    #251608 - perxi - stripe: Company multi-step registration, create route to get public stripe token

--- routes/web.php
+++ routes/web.php
@@ -165,3 +165,8 @@ Route::group(['prefix' => 'messages'], function () {
     Route::get('{id}', ['as' => 'messages.show', 'uses' => 'MessagesController@show']);
     Route::put('{id}', ['as' => 'messages.update', 'uses' => 'MessagesController@update']);
 });
+
+// Get stripe public key
+Route::get('/stripe_pk', function (){
+	return env('STRIPE_PK');
+})->name('stripe_pk');
