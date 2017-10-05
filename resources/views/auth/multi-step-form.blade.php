5a8b12df5775161829c92bd1364815baa2386d07
On Staging Server <daniel.ahern@exults.com>
2017-10-04 21:46:30 -0400

    fix spelling error

--- resources/views/auth/multi-step-form.blade.php
+++ resources/views/auth/multi-step-form.blade.php
@@ -182,7 +182,7 @@
                                               <div class="form-group col-md-12">
                                                   <div class="col-md-12">
                                                     <label for="card-element">
-                                                      Credit Cart Number
+                                                      Credit Card Number
                                                     </label>
                                                     <div id="card-element">
                                                     </div>
