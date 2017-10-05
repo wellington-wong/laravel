c7f78491808898913a15ef4ceb93a15892be97f0
dan <daniel.ahern@exults.com>
2017-10-06 05:24:44 +0900

    add filemtime to some js files

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,8 +1,8 @@
     <!-- Scripts -->
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/manifest.js') }}"></script>
     <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/vendor.js') }}"></script>
-    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/app.js') }}"></script>
-    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/all.js') }}"></script>
+    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/app.js') }}?v={{ File::lastModified( public_path('js/app.js'))  }}"></script>
+    <script src="https://app.{{ env('DOMAIN') }}{{ mix('/js/all.js') }}?v={{ File::lastModified( public_path('js/all.js'))  }}"></script>
 	<script src="https://js.stripe.com/v3/"></script>
 
     @yield('js')
