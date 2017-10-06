3ce4fc6a8a847258dedb60b6a2cdf4609e4e208d
wellington <wellington@exults.com>
2017-10-07 03:20:08 +0800

    #237118: Perxi - Post-Launch Fixes, add stripe credit card input on update credit card page

--- resources/views/company/update-card.blade.php
+++ resources/views/company/update-card.blade.php
@@ -11,16 +11,23 @@
         </div>
 
         <div class="row">
-            <div class="col-md-8 no-padding-lr company-profile-left">
+            <div class="col-md-12 no-padding-lr company-profile-left">
                 {{ Form::open(['route'=>'post-company-update', 'id' => 'update-company-form', 'enctype' => 'multipart/form-data']) }}
 
-                <div class="col-md-6">
-                    <label>Company Name</label>
-                    {{ Form::text('company_name') }}
+                <div class="col-md-12 form-group">
+                    <div class="form-control">
+                        {{ isset(auth()->user()->card_brand) ? auth()->user()->card_brand : 'Credit card' }} ending in {{ isset(auth()->user()->card_last_four) ? auth()->user()->card_last_four : 'N/A' }}
+                    </div>
                 </div>
 
-
-
+                <div class="col-md-12 form-group">       
+                    <label for="card-element">
+                      New Credit Card Number
+                    </label>
+                    <div id="card-element">
+                    </div>
+                    <div id="card-errors" class="hidden" role="alert"></div>
+                </div>
 
                 <div class="form-group col-md-12 text-right">
                     {{ Form::submit('Update', ['class' => 'btn btn-primary button-responsive-100']) }}
