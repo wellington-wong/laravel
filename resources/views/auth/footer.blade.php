df671f09602f5fa1646f00a27323d8e786fe0a72
wellington <wellington@exults.com>
2017-10-10 07:43:58 +0800

    #237118: Perxi - Apply new layout of perxi registration, apply social media buttons on footer

--- resources/views/auth/footer.blade.php
+++ resources/views/auth/footer.blade.php
@@ -27,16 +27,14 @@
                             <li><a href="#">List Item in Footer</a></li>
                             <li><a href="#">List Item in Footer</a></li>
                         </ul>
-                    </div>     
-                    <div class="col-md-2 footer-list col-md-offset-4">
-                        <h4>Headline 3</h4>
+                    </div>-->  
+                    <div class="col-md-2 footer-list col-md-offset-6">
                         <ul>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                         </ul>
-                    </div>-->
+                        <figure class="logo-footer col-xs-3 col-md-2"><a href="{{ route('login') }}" title="Referrals"><img src="{{ isset($_company->logo) ? $_company->logo : '/images/logo-main.png' }}" alt=""></a></figure>
+                    </div>
                 </div>
                 <div class="row">
                     <div class="middle-content">
