63d0482be752f77f3f31015a70a114e36b5d7e8f
wellington <wellington@exults.com>
2017-10-10 04:52:02 +0800

    #237118: Perxi - Apply new layout of perxi registration, update navbar cs

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -8,11 +8,11 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <figure class="logo col-xs-4 col-md-3"><a href="{{ route('login') }}" title="Referrals"><img src="{{ isset($_company->logo) ? $_company->logo : '/images/logo-main.png' }}" alt=""></a></figure>
+                        <figure class="logo col-xs-3 col-md-2"><a href="{{ route('login') }}" title="Referrals"><img src="{{ isset($_company->logo) ? $_company->logo : '/images/logo-main.png' }}" alt=""></a></figure>
                         <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
-                            <ul class="nav navbar-nav navbar-right navbar-bottom-right">
-                                <li><a href="{{ route('login') }}">Login</a></li>
-                                <li><a href="{{ route('register') }}">Register</a></li>
+                            <ul class="nav navbar-nav navbar-right navbar-bottom-right-access">
+                                <li><a href="{{ route('login') }}" class="login-link">Login</a></li>
+                                <li><a href="{{ route('register') }}" class="register-link">Register</a></li>
                             </ul>
                             <ul class="nav navbar-nav navbar-right navbar-bottom-right">
                                 <li><a href="{{ route('how-it-works') }}">How it Works</a></li>
