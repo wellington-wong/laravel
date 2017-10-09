e52d873d077ffea8193abb88778aa92a88e5bfc6
wellington <wellington@exults.com>
2017-10-10 03:41:09 +0800

    #237118: Perxi - Apply new layout of perxi registration, update footer services and contact links

--- resources/views/auth/footer.blade.php
+++ resources/views/auth/footer.blade.php
@@ -2,21 +2,21 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-2 footer-list">
-                        <h4>Headline 3</h4>
+                        <h4>Services</h4>
                         <ul>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
+                            <li><a href="#">How it Works</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Log In</a></li>
                         </ul>
                     </div>  
                     <div class="col-md-2 footer-list">
-                        <h4>Headline 3</h4>
+                        <h4>Contact</h4>
                         <ul>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
-                            <li><a href="#">List Item in Footer</a></li>
+                            <li><a href="#">(866) 808-9902</a></li>
+                            <li><a href="#">info@perxi.com</a></li>
                         </ul>
                     </div>         
                     <!--<div class="col-md-2 footer-list">
