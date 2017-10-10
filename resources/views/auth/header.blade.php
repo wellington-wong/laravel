8f0e596fe5d0d5155445ab83fbdacdce9beee9b9
wellington <wellington@exults.com>
2017-10-11 06:55:21 +0800

    #237118: Perxi - Apply new layout of perxi registration, update navbar menu

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -10,16 +10,16 @@
                         </button>
                         <figure class="logo col-xs-3 col-md-2"><a href="{{ route('login') }}" title="Referrals"><img src="{{ isset($_company->logo) ? $_company->logo : '/images/logo-main.png' }}" alt=""></a></figure>
                         <div class="col-xs-12 col-md-9 collapse navbar-collapse pull-right" id="app-navbar-collapse">
-                            <ul class="nav navbar-nav navbar-right navbar-bottom-right-access">
-                                <li><a href="{{ route('login') }}" class="login-link">Login</a></li>
-                                <li><a href="{{ route('register') }}" class="register-link">Register</a></li>
-                            </ul>
-                            <ul class="nav navbar-nav navbar-right navbar-bottom-right">
+                            <ul class="nav navbar-nav navbar-right navbar-bottom-right pull-left">
                                 <li><a href="{{ route('how-it-works') }}">How it Works</a></li>
                                 <li><a href="{{ route('features') }}">Features</a></li>
                                 <li><a href="{{ route('about-us') }}">About Us</a></li>
                                 <li><a href="{{ route('pricing') }}">Pricing</a></li>
                                 <li><a href="{{ route('contact') }}">Contact</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right navbar-bottom-right-access pull-right">
+                                <li><a href="{{ route('login') }}" class="login-link">Login</a></li>
+                                <li><a href="{{ route('register') }}" class="register-link">Register</a></li>
                             </ul>
                         </div>
                     </nav>
