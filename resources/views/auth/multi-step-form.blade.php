229b8e6e4c06093a9338abfef498d7d61431e47e
wellington <wellington@exults.com>
2017-10-11 01:15:50 +0800

    #237118: Perxi - Apply new layout of perxi registration, adjust input margin

--- resources/views/auth/multi-step-form.blade.php
+++ resources/views/auth/multi-step-form.blade.php
@@ -7,10 +7,8 @@
                                         <section>
                                             <div class="form-group-wrapper">
                                                 <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }} col-md-6">
-
                                                     <div class="col-md-12">
                                                         <input id="first-name" type="text" class="form-control" name="first_name" required autofocus placeholder="First Name *">
-
                                                         @if ($errors->has('name'))
                                                             <span class="help-block">
                                                                 <strong>{{ $errors->first('first_name') }}</strong>
@@ -20,10 +18,8 @@
                                                 </div>
 
                                                 <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }} col-md-6">
-
                                                     <div class="col-md-12">
                                                         <input id="last-name" type="text" class="form-control" name="last_name" required placeholder="Last Name *">
-
                                                         @if ($errors->has('name'))
                                                             <span class="help-block">
                                                                 <strong>{{ $errors->first('last_name') }}</strong>
@@ -37,7 +33,6 @@
                                                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} col-md-6">
                                                     <div class="col-md-12">
                                                         <input id="email" type="email" class="form-control" name="email" required placeholder="Your E-Mail *">
-
                                                         @if ($errors->has('email'))
                                                             <span class="help-block">
                                                                 <strong>{{ $errors->first('email') }}</strong>
@@ -49,7 +44,6 @@
                                                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} col-md-6">
                                                     <div class="col-md-12">
                                                         <input id="password" type="password" class="form-control" name="password" required placeholder="Password *">
-
                                                         @if ($errors->has('password'))
                                                             <span class="help-block">
                                                                 <strong>{{ $errors->first('password') }}</strong>
@@ -59,7 +53,6 @@
                                                 </div>
 
                                                 <div class="form-group col-md-6">
-
                                                     <div class="col-md-12">
                                                         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="Confirm Password *">
                                                     </div>
