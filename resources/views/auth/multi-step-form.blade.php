f87b1d59c17e904db2be04cf90192b5a3733d250
wellington <wellington@exults.com>
2017-10-11 01:10:45 +0800

    #237118: Perxi - Apply new layout of perxi registration, adjust input margin

--- resources/views/auth/multi-step-form.blade.php
+++ resources/views/auth/multi-step-form.blade.php
@@ -64,7 +64,7 @@
                                                         <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="Confirm Password *">
                                                     </div>
                                                  </div>   
-                                                 <div class="form-group col-md-12">
+                                                 <div class="form-group col-md-12 no-margin">
                                                     <label class="col-md-12 control-label password-note">*Password must be 8 characters and contain a number and a special character.</label>                                               
                                                  </div>
                                                 <div class="clearfix"></div>
