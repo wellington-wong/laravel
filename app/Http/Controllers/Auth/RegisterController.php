0c564d4c8efa4299dc1bba493a262789754e3cd8
wellington <wellington@exults.com>
2017-10-11 19:21:13 +0800

    #237118: Perxi - Add stripe monthly subscription, place creation of stripe customer to an exception

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -319,19 +319,20 @@ class RegisterController extends Controller
             // Automatically login created user
             Auth::loginUsingId($user->id, true);
 
-            // Charge credit card using stripe
-            \Stripe\Stripe::setApiKey(env('STRIPE_SK'));
-            $customer = \Stripe\Customer::create(array(
-                "description" => auth()->user()->email,
-                "source" => auth()->user()->stripe_id
-            ));
-
-            auth()->user()->stripe_id = $customer->id;
-            auth()->user()->save();
-
             $message[] = 'Congratulations! your company has been successfully created.';
 
             try {
+
+                // Charge credit card using stripe
+                \Stripe\Stripe::setApiKey(env('STRIPE_SK'));
+                $customer = \Stripe\Customer::create(array(
+                    "description" => auth()->user()->email,
+                    "source" => auth()->user()->stripe_id
+                ));
+
+                auth()->user()->stripe_id = $customer->id;
+                auth()->user()->save();
+                
                 $charge = \Stripe\Charge::create(array(
                     "amount" => 99900,
                     "currency" => "usd",
