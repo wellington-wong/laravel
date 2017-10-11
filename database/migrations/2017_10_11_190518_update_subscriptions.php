f868c829087843cdc79934da3323afd7051384de
wellington <wellington@exults.com>
2017-10-12 03:45:12 +0800

    #237118: Perxi - Add stripe monthly subscription, update subscriptions table

---
+++ database/migrations/2017_10_11_190518_update_subscriptions.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSubscriptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->integer('company_id')->after('user_id');
+            $table->string('subscription_name')->after('company_id');
+            $table->integer('amount')->after('stripe_plan');
+            if(Schema::hasColumn('subscriptions', 'name')) {
+                $table->dropColumn('name');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('name')->after('user_id');
+            if(Schema::hasColumn('subscriptions', 'company_id')) {
+                $table->dropColumn('company_id');
+            }
+            if(Schema::hasColumn('subscriptions', 'amount')) {
+                $table->dropColumn('amount');
+            }
+        });
+    }
+}
