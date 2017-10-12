9dc2a23a9eb8a93d474e667684d918810fa88297
wellington <wellington@exults.com>
2017-10-12 20:14:04 +0800

    #237118: Perxi - Create tests, update RegisterTest

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -13,11 +13,21 @@ class RegisterTest extends DuskTestCase
      *
      * @return void
      */
-    public function testRegister()
+    public function testRegisterStep1()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/register')
-                    ->assertSee('Basic package');
+                    ->assertSee('Basic package')
+                    ->assertSee('We need some basic information about you to get started.')
+                    ->assertSee('*Password must be 8 characters and contain a number and a special character.')
+                    ->assertSee('BACK')
+                    ->assertSee('NEXT')
+                    ->assertSee('Your Info')
+                    ->assertSee('Company Info')
+                    ->assertSee('Billing Information')
+                    ->assertSee('Form Builder')
+                    ->assertSee('Reward Info')
+                    ->assertSee('Review');
         });
     }
 }
