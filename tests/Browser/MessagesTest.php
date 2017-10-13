d258beee86cc5826ee7179ff71b038ede633317b
wellington <wellington@exults.com>
2017-10-13 18:45:57 +0800

    #237118: Perxi - Create tests, update MessagesTest

--- tests/Browser/MessagesTest.php
+++ tests/Browser/MessagesTest.php
@@ -5,6 +5,9 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use App\RoleUser;
+use App\Company;
 
 class MessagesTest extends DuskTestCase
 {
@@ -13,11 +16,35 @@ class MessagesTest extends DuskTestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testMessages()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+
+        // Get a valid company
+        $company = null;
+        while ($company == null) {
+            $company_id = RoleUser::inRandomOrder()->first()->company_id;
+            $company = Company::find($company_id);       
+        }
+
+        // Get a valid user
+        $user = null;
+        while ($user == null) { 
+            $user_id = RoleUser::where('company_id', $company_id)->inRandomOrder()->first()->user_id;
+            $user = User::find($user_id);     
+        }
+
+        // Visit subdomain
+        Browser::$baseUrl = 'https://'. $company->subdomain . '.' . env('DOMAIN');
+
+        $this->browse(function (Browser $browser) use($user) {
+            $browser->loginAs($user->id)
+            ->visit('/messages')
+            ->assertSee('Messages')
+            ->assertSee('Subject')
+            ->assertSee('From')
+            ->assertSee('To')
+            ->assertSee('Action')
+            ->assertSee('Compose a New Message');
         });
     }
 }
