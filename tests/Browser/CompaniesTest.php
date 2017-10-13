2eec6a05de4d33ab94dc1feba396daa547e43980
wellington <wellington@exults.com>
2017-10-14 06:51:03 +0800

    #237118: Perxi - Create tests, add Companies Test

---
+++ tests/Browser/CompaniesTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\RoleUser;
+
+class CompaniesTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testCompanies()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(RoleUser::inRandomOrder()->first()->user_id)
+                    ->visit('/companies')
+                    ->assertSee('Create Company');
+        });
+    }
+}
