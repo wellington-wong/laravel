13c7282f569cfa4c4c334716ad605a0a76e1869f
wellington <wellington@exults.com>
2017-10-14 02:54:42 +0800

    #237118: Perxi - Create tests, update ReferralViewTest

--- tests/Browser/ReferralViewTest.php
+++ tests/Browser/ReferralViewTest.php
@@ -5,6 +5,10 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+use App\Company;
+use App\RoleUser;
+use App\Referral;
 
 class ReferralViewTest extends DuskTestCase
 {
@@ -15,10 +19,34 @@ class ReferralViewTest extends DuskTestCase
      */
     public function testReferralView()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/referral/view')
+        // Get a valid company
+        $company = null;
+        while ($company == null) {
+            $company_id = RoleUser::inRandomOrder()->first()->company_id;
+            $company = Company::find($company_id);       
+        }
+
+        // Get a valid user
+        $user = null;
+        while ($user == null) { 
+            $user_id = RoleUser::where('company_id', $company_id)->inRandomOrder()->first()->user_id;
+            $user = User::find($user_id);     
+        }
+
+        // Visit subdomain
+        Browser::$baseUrl = 'https://'. $company->subdomain . '.' . env('DOMAIN');
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user->id)
+                ->visit('/referral/view/' . Referral::inRandomOrder()->first()->id)
                 ->assertSee('Referral History')
-                ->assertSee('Referral Details');
+                ->assertSee('Date')
+                ->assertSee('Referral Id')
+                ->assertSee('Details')
+                ->assertSee('Referral Details')
+                ->assertSee('ID')
+                ->assertSee('Label')
+                ->assertSee('Value');
         });
     }
 }
