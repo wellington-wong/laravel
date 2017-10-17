ba74b25980147100c918969ccfa315c6a94b62a3
wellington <wellington@exults.com>
2017-10-17 18:34:49 +0800

    #251608 - perxi - stripe: Update stripe test form

--- resources/views/stripe/test.blade.php
+++ resources/views/stripe/test.blade.php
@@ -4,13 +4,30 @@
 
 @section('content')
 
-	{{ Form::open(['route' => 'post-stripe-test' , 'id' => 'stripe-test-form', 'class' => 'stripe-test-form']) }}
-        <div class="form-group col-md-12">
-            <div class="col-md-12">
-              <div id="card-element"></div>
-              <div id="card-errors" role="alert"></div>
-            </div>
-        </div>
-	{{ Form::close() }}
+    <div class="row">
+	   	<div class="col-md-12">
+    		@include('layouts.page-header', ['header' => 'Stripe Test Charge', 'col' => 12])
+   		</div>
+    </div>
+
+    <div class="row">
+		{{ Form::open(['route' => 'post-stripe-test' , 'id' => 'stripe-test-form', 'class' => 'stripe-test-form']) }}
+	        <div class="form-group">
+	            <div class="col-md-12">
+	              <div id="card-element"></div>
+	              <div id="card-errors" role="alert"></div>
+	            </div>
+	        </div>
+	        <div class="form-group col-md-6">
+	            <label>Amount to charge *</label>
+	            <div>
+	                {{ Form::text('amount', '', array('class' => 'form-control', 'placeholder' => 'Amount to charge *')) }}
+	            </div>
+	        </div>
+	        <div class="form-group col-md-12 text-right">
+	        	{{ Form::submit('Submit', ['class' => 'btn btn-primary button-responsive-100 submit-charge']) }}
+	        </div>
+		{{ Form::close() }}
+    </div>
 
 @endsection
