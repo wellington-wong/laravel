62aedd0d4ea2060cb2a3e8c5643ca5f7f96bd45d
wellington <wellington@exults.com>
2017-10-18 05:16:43 +0800

    #237118 - Perxi: Add current to companies

---
+++ database/migrations/2017_10_17_211411_add_current_to_companies.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCurrentToCompanies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->integer('current')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            if(Schema::hasColumn('companies', 'current')) {
+                $table->dropColumn('current');
+            }
+        });
+    }
+}
