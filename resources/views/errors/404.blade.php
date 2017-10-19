e26952da3e73cafc63ee6a790297ed1b39556ec3
wellington <wellington@exults.com>
2017-10-20 04:25:33 +0800

    #252223 - super user delete users and companies: update 404 and 403 blade templates

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,28 +4,29 @@
     <form class="render-wrap"></form>
         <!-- Start Header -->
         <header>
-            <div class="row header-right">
-                <div class="col-xs-12 col-md-12 pull-right">
-                    <ul class="nav navbar-nav navbar-right navbar-top-right">
-                        <li><a href="{{ route('home') }}">Home</a></li>
-                    </ul>
-                </div>
-            </div>
         @include('auth.header')
         </header>
         <!-- End Header -->
 
         <!-- Start Main -->
         <main>
-            <div class="row top-content-wrapper">
-                <div class="top-content text-center">
-                    <span>404 Not Found</span>
-                </div>
-            </div>
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 main-content">
-                        <p class="text-center">The requested URL <em>{{ Request::url() }}</em> was not found on this server.
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="row top-content-wrapper">
+                                    <div class="top-content text-center">
+                                        <span>404 Not Found</span>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <p class="text-center">The requested URL <em>{{ Request::url() }}</em> was not found on this server.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::post('/company/register', 'Auth\CompanyController@register')->name('compa
 Route::get('/company/credit-card/update', 'CompanyController@updateCard')->name('company-update-card');
 Route::post('/company/credit-card/update', 'CompanyController@postUpdateCard')->name('company-update-card');
 Route::post('/company/delete/{id}', ['uses' => 'CompanyController@postDeleteCompany', 'middleware' => ['role:globalAdmin']])->name('company-delete');
+Route::get('/company/delete/{id}', ['uses' => 'CompanyController@postDeleteCompany', 'middleware' => ['role:globalAdmin']])->name('company-delete');
 
 // Referral Routes
 Route::get('/form/{id}', 'ReferralController@formJson')->name('form-json');
