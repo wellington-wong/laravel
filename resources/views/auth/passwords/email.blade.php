a201e8b31abc999aeede301ad56f5a9ab5599bae
wellington <wellington@exults.com>
2017-10-20 04:40:56 +0800

    #252223 - super user delete users and companies: update layout of forget and reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,30 +2,21 @@
 @include('auth.document-top')
         <!-- Start Header -->
         <header>
-            <div class="row header-right">
-                <div class="col-xs-12 col-md-12 pull-right">
-                    <ul class="nav navbar-nav navbar-right navbar-top-right">
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                    </ul>
-                </div>
-            </div>
         @include('auth.header')
         </header>
         <!-- End Header -->
 
         <!-- Start Main -->
         <main>
-            <div class="row top-content-wrapper">
-                <div class="top-content text-center">
-                    <span>Enter your email address to reset the password</span>
-                </div>
-            </div>
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 main-content">
                         <div class="panel panel-default">
-                            <div class="panel-heading">Reset Password</div>
                             <div class="panel-body">
+                                <div class="top-content text-center">
+                                    <span>Enter your email address to reset the password.</span>                                    
+                                </div>
+                                <div>&nbsp;</div>
                                 @if (session('status'))
                                     <div class="alert alert-success">
                                         {{ session('status') }}
