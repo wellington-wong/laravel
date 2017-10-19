ef798f6b6a635df8573a371493b46877af3469f7
wellington <wellington@exults.com>
2017-10-20 03:00:15 +0800

    #252223 - super user delete users and companies: update js

--- public/js/app.js
+++ public/js/app.js
@@ -21565,20 +21565,26 @@ $(function () {
 
 	// COMPANY
 
-	// Delete company
-	var allCompanies_modal = $('.all-companies-modal #incentful-modal');
-	$('.delete-company').on('click', function () {
-		console.log('delete company');
-		allCompanies_modal.find('.modal-title').text('Delete Company Confirmation');
-		allCompanies_modal.find('.modal-body').html('Are you sure you want to delete this company?');
-		allCompanies_modal.modal('show');
-	});
-	allCompanies_modal.find('.btn.submit').on('click', function () {
-		ajaxHelper("/company/delete/", data, "POST", statusCallback);
-	}).text('Yes').addClass('btn-primary');
-	allCompanies_modal.find('.btn.cancel').on('click', function () {
-		allCompanies_modal.modal('hide');
-	}).text('No').addClass('btn-danger');
+	if ($('.all-companies').length) {
+		// Delete company
+		var allCompanies_modal = $('.all-companies-modal #incentful-modal');
+		var cid;
+		$('.delete-company').on('click', function () {
+			allCompanies_modal.find('.modal-title').text('Delete Company Confirmation');
+			allCompanies_modal.find('.modal-body').html('Are you sure you want to delete this company?');
+			allCompanies_modal.modal('show');
+			cid = $(this).data('id');
+		});
+		allCompanies_modal.find('.btn.submit').on('click', function () {
+			ajaxHelper("/company/delete/" + cid, [], "POST", function (data) {
+				console.log(data);
+				//location.reload();
+			});
+		}).text('Yes').addClass('btn-primary');
+		allCompanies_modal.find('.btn.cancel').on('click', function () {
+			allCompanies_modal.modal('hide');
+		}).text('No').addClass('btn-danger');
+	}
 
 	// END COMPANY
 });
