8b9d3dd62d2b171212db9eaab7991b60f1ffb6fb
wellington <wellington@exults.com>
2018-01-04 18:40:23 +0800

    #255986: Perxi - ask for reviews, add view title

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 
     <div class="container-fluid">
+        <div class="row">
+        @include('layouts.page-header', ['header' => 'Reviews', 'col' => 12])
+        </div>
         <div class="clearfix"></div>
     </div>
 
