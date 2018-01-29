c7d2c37f2fcf41bf1e98f1b6ab4b36af19169dd0
wellington <wellington@exults.com>
2018-01-29 19:41:56 +0800

    #255986: Perxi - ask for reviews, Add review push code

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -3,6 +3,115 @@
 @section('pageTitle', 'Reviews')
 
 @section('content')
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min.js"></script>
+
+
+
+<div class="feedback-form" data-bind="visible: showFeedbackForm">
+
+<form data-bind="submit: save" method="post">
+
+
+<input name="name" id="name" value="" placeholder="Name" data-bind="value: reviewer_name" type="text">
+<input name="email" id="email" value="" placeholder="Email" data-bind="value: reviewer_email" type="text">
+<textarea name="message" id="input-message" placeholder="Message" data-bind="value: review_text"></textarea>
+
+
+<input data-bind="click: save" value="Send!" type="submit">
+
+
+</div>
+<div style="display: none;" class="thank-you" data-bind="visible: showThankYouMessage">Thank you for your feedback!</div><!-- Begin ReviewPush Feedback Embed -->
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+<h3>Leave Feedback</h3>
+<form method="post" data-bind="visible: showFeedbackForm, submit: save">
+  <div style="display: none;" class="location-popup" data-bind="visible: show_location_list">
+    <div data-bind="foreach: location_list"></div>
+    <a href="#" data-bind="click: hideLocationList">Close</a>
+  </div>
+
+  <div style="display: none;" data-bind="visible: has_selected_location">
+    <div><a href="#" data-bind="click: toggleLocationList">Wrong Location?</a></div>
+    <div>
+      <strong>Location:</strong>
+      <div>
+        <div data-bind="text: selected_location().name"></div>
+        <div data-bind="text: selected_location().address"></div>
+        <div>
+          <span data-bind="text: selected_location().city"></span>,
+          <span data-bind="text: selected_location().state"></span>
+          <span data-bind="text: selected_location().zip"></span>
+        </div>
+        <div data-bind="text: selected_location().phone"></div>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <a href="#" class="star full" data-bind="click: function(data, event) { setRating(1); }, css: { full: rating() >= 1 }"></a>
+    <a href="#" class="star full" data-bind="click: function(data, event) { setRating(2); }, css: { full: rating() >= 2 }"></a>
+    <a href="#" class="star full" data-bind="click: function(data, event) { setRating(3); }, css: { full: rating() >= 3 }"></a>
+    <a href="#" class="star full" data-bind="click: function(data, event) { setRating(4); }, css: { full: rating() >= 4 }"></a>
+    <a href="#" class="star full" data-bind="click: function(data, event) { setRating(5); }, css: { full: rating() == 5 }"></a>
+  </div>
+
+  <div>
+    <input name="name" id="name" value="" placeholder="Name" data-bind="value: reviewer_name" type="text">
+  </div>
+
+  <div>
+    <input name="email" id="email" value="" placeholder="Email" data-bind="value: reviewer_email" type="text">
+  </div>
+
+  <div>
+    <textarea name="message" id="input-message" placeholder="Message" data-bind="value: review_text"></textarea>
+  </div>
+
+  <div>
+    <input name="submit" data-bind="click: save" id="submit_btn" value="Send Message!" type="submit">
+  </div>
+</form>
+
+<div style="display: none;" data-bind="visible: showThankYouMessage">
+  Thank you for your feedback! Please be assured that it will be routed to the right person in our organization. We take all feedback very seriously. 
+</div>
+
+<script>
+  var api_key = 'API KEY';
+  var default_rating = '5';
+  var location_results = 5;
+</script>
+<script type="text/javascript" src="//go.reviewpush.com/go/feedback/js/feedback_embed.js"></script>
+<style type="text/css">
+  .star {
+    background-image:url(http://go.reviewpush.com/img/blue-star-empty.png);
+    display:inline-block;
+    height:18px;
+    width:18px;
+  }
+  .star.full {
+    background-image:url(http://go.reviewpush.com/img/blue-star-full.png);
+  }
+  .location-popup {
+    position: absolute;
+    z-index: 99999;
+    background-color: white;
+    border: 1px solid #aaa;
+    padding: 20px;
+  }
+  .location-row {
+    border-bottom: 1px solid #aaa;
+    padding-top: 6px;
+    padding-bottom: 6px;
+    cursor: pointer;
+  }
+</style>
+
+<!-- End ReviewPush Feedback Embed -->
+
+
 
     <div class="container-fluid">
         <div class="row">
