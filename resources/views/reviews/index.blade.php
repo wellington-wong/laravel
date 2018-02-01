ebad33bb24130b996f86e48a94c5b2de728933db
wellington <wellington@exults.com>
2018-02-01 23:58:00 +0800

    #255986: Perxi - ask for reviews, add rating to review push template

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -60,16 +60,18 @@
     <textarea name="message" class="form-control" id="input-message" placeholder="Message" data-bind="value: review_text"></textarea>
   </div>
 
-  <!-- Rating -->
-  <div class="form-group">
-    <label class="col-md-3 control-label" for="message">Your rating</label>
-    <div class="col-md-9">
-      <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
+  <div class="row">
+    <div class="btn-group col-md-4">
+      <input name="submit" class="btn btn-primary" data-bind="click: save" id="submit_btn" value="Send Message!" type="submit">
     </div>
-  </div>
 
-  <div class="btn-group">
-    <input name="submit" class="btn btn-primary" data-bind="click: save" id="submit_btn" value="Send Message!" type="submit">
+    <!-- Rating -->
+    <div class="form-group col-md-8 text-right">
+      <label class="col-md-3 control-label" for="message">Rating</label>
+      <div>
+        <input id="input-21e" value="0" type="number" class="rating" min=0 max=5 step=0.5 data-size="xs" >
+      </div>
+    </div>
   </div>
 
 <div class="reviews-container">
