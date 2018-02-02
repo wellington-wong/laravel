fd451a223f19a590b93b0cd663e63de61849965f
wellington <wellington@exults.com>
2018-02-02 22:37:22 +0800

    #255986: Perxi - ask for reviews, Add authentication to ReviewsController

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class ReviewsController extends Controller
 {
 
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+
+		$this->middleware('auth');
+
+	}
+
     public function getIndex (Request $request) {
 
     	return view ('reviews.index');
