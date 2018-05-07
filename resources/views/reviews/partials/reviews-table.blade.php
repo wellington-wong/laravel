4a79a30030501f4e78ca483dce1a94d56e14b11b
wellington <wellington@exults.com>
2018-05-08 07:20:21 +0800

    #255986: Perxi - ask for reviews, Update reviews table

--- resources/views/reviews/partials/reviews-table.blade.php
+++ resources/views/reviews/partials/reviews-table.blade.php
@@ -11,21 +11,9 @@
                     </thead> 
                     <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>
                     @foreach($reviews as $r)
-
-                        <tr>
-                            @if (auth()->user()->hasRole(['admin', 'superAdmin', 'globalAdmin']))<td><a href="{{ route('view-user', isset($r->referred->id) ? $r->referred->id : 0) }}">{{ isset($r->referred->display_name) ? $r->referred->display_name : '' }}</a></td>
-                            @else <td>{{ $r->referred->display_name }}</td>@endif
-                            @if (auth()->user()->hasRole(['admin', 'superAdmin', 'globalAdmin']))<td><a href="{{ route('view-user', isset($r->referrer->id) ? $r->referrer->id : 0) }}">{{ isset($r->referrer->display_name) ? $r->referrer->display_name : '' }}</a></td>@endif     
-                            <td class="referral-status" data-id="{{ $r->id }}"></td>
-                            <td>{{ isset($r->created_at) ? $r->created_at->format('m/d/y') : '' }}</td>
-                            @if (auth()->user()->hasRole(['admin', 'superAdmin', 'globalAdmin']))<td><a href="{{ route('referral-view', $r->id) }}">History</a></td>@endif
-                            @if (auth()->user()->hasRole('member'))<td class="view-details"><a href="{{ route('referral-view', $r->id) }}" class="btn btn-primary">History</a></td>@endif
-                            @role(['globalAdmin'])<td class="referral-actions">
-                                <a href="javascript:void(0)" class="transfer" title="Delete" data-url="{{ route('referral-transfer', $r->id) }}" data-rname="{{ isset($r->referred) ? $r->referred->getName() : null }}"><i class="fa fa-exchange fa-1x" aria-hidden="true"></i></a>&nbsp;&nbsp;
-                                <a href="javascript:void(0)" class="delete" title="Transfer Referral" data-url="{{ route('referral-delete', $r->id) }}" data-rname="{{ isset($r->referred) ? $r->referred->getName() : null }}"><i class="fa fa-trash fa-1x" aria-hidden="true"></i></a>
-                            </td>@endrole
-                        </tr>
-                        <tr class="tr-spacer"><td colspan=5></td></tr>
+                    <tr>
+                    </tr>
+                    <tr class="tr-spacer"><td colspan=5></td></tr>
                     @endforeach
                     @if (!count($reviews))<tr><td colspan="5">No referrals found.</td></tr>@endif
                 </table>
