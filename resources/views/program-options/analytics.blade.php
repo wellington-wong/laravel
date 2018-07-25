763521201de6887783b97bde8a5e0060b4ce10f1
wellington <wellington@exults.com>
2018-07-25 08:19:17 -0400

    #267771: Perxi Monthly Stats - Add analytics.blade.pho

---
+++ resources/views/program-options/analytics.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Users')
+
+@section('content')
+    <div class="container-fluid analytics-wrapper">
+        <div class="row">
+        @include('layouts.page-header', ['header' => 'Analytics', 'col' => 12])
+        </div>
+    </div>
+
+@endsection
