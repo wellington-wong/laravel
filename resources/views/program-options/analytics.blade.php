c5c3a1cb998f4f4cc699e3d91775f06ae2af138f
wellington <wellington@exults.com>
2018-07-25 17:14:50 -0400

    #267771: Perxi Monthly Stats - Add analytics table

--- resources/views/program-options/analytics.blade.php
+++ resources/views/program-options/analytics.blade.php
@@ -9,4 +9,29 @@
         </div>
     </div>
 
+        <div class="row">
+            <div class="col-md-12 table-referral-wrapper table-wrapper {{ auth()->user()->hasRole('member') ? 'member-referrals' : '' }}">
+                <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
+                    <thead>
+                        <tr>
+                            <th><a href="">New referral source accounts</th>
+                            <th><a href="">Referred by referral sources</th>
+                            <th>Converted referrals</th>
+                        </tr>
+                    </thead> 
+                    <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>
+                    
+                        <tr>
+                           <td></td>                          
+                           <td></td>                          
+                           <td></td>                          
+                        </tr>
+                        <tr class="tr-spacer"><td colspan=5></td></tr>
+                </table>
+             </div>
+        </div>
+        <div class="hidden members-select">{{ Form::select('as_member', ($_company->members()->pluck('name', 'id') ? ['0' => 'Please select a member'] +  $_company->members()->pluck('name', 'id')->toArray() : [] ), '', ['class' => 'form-control']) }}</div>
+        @include('layouts.modal')
+    </div>
+
 @endsection
