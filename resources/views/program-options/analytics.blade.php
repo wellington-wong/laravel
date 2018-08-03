6c205cbf1f6600ea173cc53f00fd2dd10cff80b1
wellington <wellington@exults.com>
2018-08-03 14:47:07 -0400

    #267390: Perxi Monthly Stats - Update analytics layout

--- resources/views/program-options/analytics.blade.php
+++ resources/views/program-options/analytics.blade.php
@@ -3,35 +3,34 @@
 @section('pageTitle', 'Analytics')
 
 @section('content')
-    <div class="container-fluid analytics-wrapper">
+    <div class="container-fluid referrals-analytics">
         <div class="row">
-        @include('layouts.page-header', ['header' => 'Analytics', 'col' => 12])
-        </div>
-    </div>
+            @include('layouts.page-header', ['header' => 'Analytics', 'col' => 12])
+            </div>
 
-        <div class="row">
-            <div class="col-md-12 table-referral-wrapper table-wrapper {{ auth()->user()->hasRole('member') ? 'member-referrals' : '' }}">
-                <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
-                    <thead>
-                        <tr>
-                            <th><a href="">New referral source accounts</th>
-                            <th><a href="">Referred by referral sources</th>
-                            <th>Converted referrals</th>
-                        </tr>
-                    </thead> 
-                    <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>
-                    
+            <div class="row">
+                <div class="col-md-12 table-referral-wrapper table-wrapper {{ auth()->user()->hasRole('member') ? 'member-referrals' : '' }}">
+                    <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
+                        <thead>
+                            <tr>
+                                <th><a href="">New referral source accounts</th>
+                                <th><a href="">Referred by referral sources</th>
+                                <th>Converted referrals</th>
+                            </tr>
+                        </thead> 
+                        <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>                    
                         <tr>
                            <td></td>                          
                            <td></td>                          
                            <td>{{ $referrals->where('status', 3)->count() }}</td>                          
                         </tr>
                         <tr class="tr-spacer"><td colspan=5></td></tr>
-                </table>
-             </div>
+                    </table>
+                 </div>
+            </div>
+            <div class="hidden members-select">{{ Form::select('as_member', ($_company->members()->pluck('name', 'id') ? ['0' => 'Please select a member'] +  $_company->members()->pluck('name', 'id')->toArray() : [] ), '', ['class' => 'form-control']) }}</div>
+            @include('layouts.modal')
         </div>
-        <div class="hidden members-select">{{ Form::select('as_member', ($_company->members()->pluck('name', 'id') ? ['0' => 'Please select a member'] +  $_company->members()->pluck('name', 'id')->toArray() : [] ), '', ['class' => 'form-control']) }}</div>
-        @include('layouts.modal')
     </div>
 
 @endsection
