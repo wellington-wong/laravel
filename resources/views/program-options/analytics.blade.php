4f0c8d119b499151b298d7842ed91f4b2bf623ed
wellington <wellington@exults.com>
2018-08-06 06:49:21 -0400

    #267390: Perxi Monthly Stats - Add table for analytics

--- resources/views/program-options/analytics.blade.php
+++ resources/views/program-options/analytics.blade.php
@@ -13,9 +13,9 @@
                     <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
                         <thead>
                             <tr>
-                                <th><a href="">New referral source accounts</th>
-                                <th><a href="">Referred by referral sources</th>
-                                <th>Converted referrals</th>
+                                <th><a href="">New referral source accounts</a></th>
+                                <th><a href="">Referred by referral sources</a></th>
+                                <th><a href="">Converted referrals</a></th>
                             </tr>
                         </thead> 
                         <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>                    
@@ -34,6 +34,38 @@
                         <thead>
                             <tr>
                                 <th><a href="">New referral source accounts</th>
+                            </tr>
+                        </thead> 
+                        <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>                    
+                        <tr>
+                           <td></td>                            
+                        </tr>
+                        <tr class="tr-spacer"><td colspan=5></td></tr>
+                    </table>
+                 </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 table-referral-wrapper table-wrapper {{ auth()->user()->hasRole('member') ? 'member-referrals' : '' }}">
+                    <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
+                        <thead>
+                            <tr>
+                                <th><a href="">Referred by referral sources</th>
+                            </tr>
+                        </thead> 
+                        <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>                    
+                        <tr>
+                           <td></td>                            
+                        </tr>
+                        <tr class="tr-spacer"><td colspan=5></td></tr>
+                    </table>
+                 </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 table-referral-wrapper table-wrapper {{ auth()->user()->hasRole('member') ? 'member-referrals' : '' }}">
+                    <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
+                        <thead>
+                            <tr>
+                                <th><a href="">Converted referrals</th>
                             </tr>
                         </thead> 
                         <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>                    
