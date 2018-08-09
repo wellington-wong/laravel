e08a62e8f2e5737efd4cfea2bc148327bc27ed38
wellington <wellington@exults.com>
2018-08-09 06:31:01 -0400

    #267390: Perxi Monthly Stats - Update layout

--- resources/views/program-options/analytics.blade.php
+++ resources/views/program-options/analytics.blade.php
@@ -9,10 +9,10 @@
             </div>
             <div class="row">
                 <div class="col-md-12 table-referral-wrapper table-wrapper {{ auth()->user()->hasRole('member') ? 'member-referrals' : '' }}">
-                    <div class="col-md-6">
+                    <div class="col-md-8 no-padding-lr">
                         <label>Summary</label>
                     </div>
-                    <div class="col-md-6 filter-item date-range analytics-calendar">
+                    <div class="col-md-4 filter-item date-range analytics-calendar no-padding-lr">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
                         {{ Form::text('daterange_analytics', app('request')->get('date') ?: date('F') . ' ' . date('Y'), ['class' => 'form-control text', 'data-query' => (isset($param->daterange) ? $param->daterange : ''), 'data-url' => route('get-program-options-analytics')] ) }}            
                         <i class="fa fa-angle-down" aria-hidden="true"></i>          
@@ -22,9 +22,9 @@
                     <table class="table table-referral tablesaw tablesaw-stack table-custom" data-tablesaw-mode="stack">
                         <thead>
                             <tr>
-                                <th><a href="">New referral source accounts</a></th>
-                                <th><a href="">Referred by referral sources</a></th>
-                                <th><a href="">Converted referrals</a></th>
+                                <th><a href="#referral-source">New referral source accounts</a></th>
+                                <th><a href="#referral-referred">Referred by referral sources</a></th>
+                                <th><a href="#referral-converted">Converted referrals</a></th>
                             </tr>
                         </thead> 
                         <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>                    
