8f78499231111d30e63e5211da13c8b0ccd1d0a4
wellington <wellington@exults.com>
2018-08-09 08:22:48 -0400

    #267390: Perxi Monthly Stats - Update date filter default value

--- resources/views/program-options/analytics.blade.php
+++ resources/views/program-options/analytics.blade.php
@@ -14,7 +14,7 @@
                     </div>
                     <div class="col-md-4 filter-item date-range analytics-calendar no-padding-lr">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
-                        {{ Form::text('daterange_analytics', app('request')->get('date') ?: date('F') . ' ' . date('Y'), ['class' => 'form-control text', 'data-query' => (isset($param->daterange) ? $param->daterange : ''), 'data-url' => route('get-program-options-analytics')] ) }}            
+                        {{ Form::text('daterange_analytics', date("F Y", mktime(0, 0, 0, $selectionMonth + 1, 0, $selectionYear)), ['class' => 'form-control text', 'data-query' => (isset($param->daterange) ? $param->daterange : ''), 'data-url' => route('get-program-options-analytics')] ) }}            
                         <i class="fa fa-angle-down" aria-hidden="true"></i>          
                     </div> 
                     <!--<label for="startDate">Date :</label>
