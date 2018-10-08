c130e58cdab2d50273b7b7935de31ba3ccf95ebf
wellington <wellington@exults.com>
2018-10-08 16:48:59 -0400

    Update user reviews, add status code

--- resources/views/reviews/partials/reviews-table2.blade.php
+++ resources/views/reviews/partials/reviews-table2.blade.php
@@ -7,6 +7,7 @@
                             <th><a href="">Submitted By <i class="fa" aria-hidden="true"></i></a></th>
                             <th><a href="">URL <i class="fa" aria-hidden="true"></i></a></th>
                             <th><a href="">Screenshot <i class="fa" aria-hidden="true"></i></a></th>
+                            <!--<th><a href="">Status <i class="fa" aria-hidden="true"></i></a></th>-->
                         </tr>
                     </thead> 
                     <tr class="tr-spacer"><td colspan=5 style="border: 0; height:10px;"></td></tr>
@@ -20,6 +21,7 @@
                             <a href="#" class="review-screenshot-thumb" data-screenshoturl="{{ url($r->screenshot) }}"><img width="30" src="{{ url($r->screenshot) }}" /></a>
                             @endif
                         </td>
+                        <!--<td data-id="2162" class="referral-status"><div data-toggle="dropdown" data-status="1" class="form-control current-referral-status" aria-expanded="false">Submitted</div> <ul class="dropdown-menu"><li><a href="javascript:void(0)" data-status="1">Submitted</a></li> <li><a href="javascript:void(0)" data-status="2">Approved</a></li> <li><a href="javascript:void(0)" data-status="3">Reward Sent</a></li> <li><a href="javascript:void(0)" data-status="4">Denied</a></li></ul> <i aria-hidden="true" class="fa fa-angle-down"></i></td>-->
                     </tr>
                     <tr class="tr-spacer"><td colspan=5></td></tr>
                     @endforeach
