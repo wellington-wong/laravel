32155a9c34f5e22c1a760d2867dc0d4287253c2c
wellington <wellington@exults.com>
2018-10-08 12:18:03 -0400

    Create user_reviews table

---
+++ database/migrations/2018_10_08_115255_create_user_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('url');
+            $table->string('screenshot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_reviews');
+    }
+}
