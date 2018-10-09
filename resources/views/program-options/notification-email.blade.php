b41f8d88c61e57553ddf757ccb983f3674537bc7
wellington <wellington@exults.com>
2018-10-08 22:58:46 -0400

    Update user reviews, notification email

--- resources/views/program-options/notification-email.blade.php
+++ resources/views/program-options/notification-email.blade.php
@@ -30,7 +30,7 @@
         <span id="renderHtml" class="btn btn-link" >Render HTML</span>
         <span id="defaultHtml" class="btn btn-link" >Change to Example HTML (over-writes but doesn't save current HTML)</span>
         
-        @if ($emailTemplateType != 1 && $emailTemplateType != 6)
+        @if ($emailTemplateType != 1 && $emailTemplateType != 6 && $emailTemplateType != 8)
         <div class="referrer-data placeholder-name">
             <label>Available referrer data:</label>
             <ul class="list-inline">
@@ -49,17 +49,19 @@
                 <li><a href="javascript:void(0);" data-var="referred_address">&#123; &#123; referred_address }}</a></li>
             </ul>
         </div>
-        @else
-        <div class="referred-data placeholder-name">
-            <label>Available user data:</label>
-            <ul class="list-inline">
-                <li><a href="javascript:void(0);" data-var="referred_name">&#123; &#123; name }}</a></li>
-                <li><a href="javascript:void(0);" data-var="referred_email">&#123; &#123; email }}</a></li>
-                <li><a href="javascript:void(0);" data-var="referred_phone">&#123; &#123; phone }}</a></li>
-                <li><a href="javascript:void(0);" data-var="referred_address">&#123; &#123; address }}</a></li>
-                <li><a href="javascript:void(0);" data-var="referred_address">&#123; &#123; password }}</a></li>
-            </ul>
-        </div>
+        @else            
+            @if ($emailTemplateType != 8)
+            <div class="referred-data placeholder-name">
+                <label>Available user data:</label>
+                <ul class="list-inline">
+                    <li><a href="javascript:void(0);" data-var="referred_name">&#123; &#123; name }}</a></li>
+                    <li><a href="javascript:void(0);" data-var="referred_email">&#123; &#123; email }}</a></li>
+                    <li><a href="javascript:void(0);" data-var="referred_phone">&#123; &#123; phone }}</a></li>
+                    <li><a href="javascript:void(0);" data-var="referred_address">&#123; &#123; address }}</a></li>
+                    <li><a href="javascript:void(0);" data-var="referred_address">&#123; &#123; password }}</a></li>
+                </ul>
+            </div>
+            @endif
         @endif
         <div class="form-group">
             <textarea class="form-control" name="email_html" id="email_html">
