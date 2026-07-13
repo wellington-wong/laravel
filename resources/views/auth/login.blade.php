fda5f226150334d345b98b289f7759fd3e9b58f6
Wellington <wellington.wong@exults.com>
2026-07-14 03:32:03 +0800

    Update prefille login account credentials

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
                                         <label for="email" class="col-md-4 control-label">E-Mail Address *</label>
 
                                         <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control" name="email" value="exults.referral.superadmin@gmail.com" required autofocus>
+                                            <input id="email" type="email" class="form-control" name="email" value="referral@gmail.com" required autofocus>
 
                                             @if ($errors->has('email'))
                                                 <span class="help-block">
