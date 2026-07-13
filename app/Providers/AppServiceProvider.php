ac2596946e06cb697601cd30f61d34a0c664c501
Wellington <wellington.wong@exults.com>
2026-07-13 09:25:33 +0800

    Fix https on form actions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (app()->environment('production')) {
-            URL::forceScheme('http1');
+            URL::forceScheme('http');
         }
     }
 
