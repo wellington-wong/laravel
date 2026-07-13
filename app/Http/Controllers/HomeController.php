b6187120e012feb0501b85ec583429107c09bb82
Wellington <wellington.wong@exults.com>
2026-07-14 02:00:21 +0800

    Optimize workflow builds

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
         $user = auth()->user();
         $_company = $request->_company;
         $hosts = explode('.', $request->getHost());
-        $shareUrl = 'https://' . (isset($_company->subdomain) ? $_company->subdomain : '') . '.' . $hosts[1] . '.' . $hosts[2];
+        $shareUrl = 'https://' . (isset($_company->subdomain) ? $_company->subdomain : '') . '.' . $hosts[1] . '.' . $hosts[2] . '.' . $hosts[3];
 
         // Get referral pending approval and reward
         $pendingReferrals = new Referral();
