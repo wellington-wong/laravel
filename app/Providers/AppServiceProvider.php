b9790d4613f86d451b0d955dc32382f527d394c1
Wellington <wellington.wong@exults.com>
2026-07-13 09:19:55 +0800

    Fix https on form actions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        if (app()->environment('production')) {
+            URL::forceScheme('http1');
+        }
     }
 
     /**
